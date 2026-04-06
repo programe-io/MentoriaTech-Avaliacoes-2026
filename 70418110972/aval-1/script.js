a6b4ccb7b0617e791ee084914460cc544f40c756
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-04-06 10:29:15 -0300

    publish(70418110972): 1

---
+++ 70418110972/aval-1/script.js
@@ -0,0 +1,6 @@
+botao.addEventListener("click", function() {
+    const msg = document.getElementById("mensagem");
+    msg.textContent = "Você clicou no botão!";
+    msg.style.color = "green";
+    msg.style.fontSize = "20px";
+});
