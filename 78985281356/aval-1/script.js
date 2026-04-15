4632a6cd784803fb152d3b3ef8371d3c6577bdfd
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-04-15 14:17:42 -0300

    publish(78985281356): 1

---
+++ 78985281356/aval-1/script.js
@@ -0,0 +1,8 @@
+function comprar() {
+    alert("BOOYAH! Ops, jogo errado... VITÓRIA REAL! Skin adquirida com sucesso!");
+    
+    // Pequeno efeito no botão ao clicar
+    const btn = document.querySelector('.btn-buy');
+    btn.innerHTML = "NA CONTA!";
+    btn.style.backgroundColor = "#4caf50";
+\}
