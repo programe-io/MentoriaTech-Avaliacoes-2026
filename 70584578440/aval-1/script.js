9843919a66a14152f6b248752341179ee9b2f2f2
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-04-15 07:39:07 -0300

    publish(70584578440): 1

---
+++ 70584578440/aval-1/script.js
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "João";
+        int idade = 20;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+
+    }
+}
