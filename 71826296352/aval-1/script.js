9f5ad8d9ac547cb68f4c43cd042c88591ee8dfe6
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-04-17 14:13:46 -0300

    publish(71826296352): 1

--- 71826296352/aval-1/script.js
+++ 71826296352/aval-1/script.js
@@ -1,3 +1,28 @@
+// Mostra uma mensagem ao carregar a página
+window.onload = function() {
+    console.log("Página carregada com sucesso!");
+};
+
+// Função para mostrar alerta
+function mostrarMensagem() {
+    alert("Você clicou no botão!");
+}
+
+// Função para mudar texto de um elemento
 function mudarTexto() {
-    document.getElementById("mensagem").innerText = "Você clicou no botão!";
+    const titulo = document.getElementById("titulo");
+    titulo.innerText = "Texto alterado com JavaScript!";
+}
+
+// Função para trocar cor de fundo
+function mudarCor() {
+    document.body.style.backgroundColor = "#dfe6e9";
+}
+
+// Função simples de contador
+let contador = 0;
+
+function incrementar() {
+    contador++;
+    document.getElementById("contador").innerText = contador;
 }
