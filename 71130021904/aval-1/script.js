6c74a4a3db556040e253514b9797eb51d282f795
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-04-20 09:27:16 -0300

    publish(71130021904): 1

---
+++ 71130021904/aval-1/script.js
@@ -0,0 +1,8 @@
+function mostrarMensagem() {
+    alert("Olá! Obrigada por visitar o site da Daniele 💜");
+}
+
+// Mensagem automática ao carregar a página
+window.onload = function() {
+    console.log("Site carregado com sucesso!");
+};
