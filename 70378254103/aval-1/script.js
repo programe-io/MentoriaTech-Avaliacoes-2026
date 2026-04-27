79666344f3501d437d6abcc1e70b8c728e8edc29
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-04-27 13:20:44 -0300

    publish(70378254103): 1

---
+++ 70378254103/aval-1/script.js
@@ -0,0 +1,21 @@
+let senhaCorreta = "4321";
+let tentativa;
+let tentativasRestantes = 3;
+
+do {
+    tentativa = prompt("Digite a senha (4 dígitos):");
+
+        if (tentativa === senhaCorreta) {
+                alert("Bem-vindo! Acesso liberado.");
+                        break;
+                            } else {
+                                    tentativasRestantes--;
+                                            
+                                                    if (tentativasRestantes > 0) {
+                                                                alert("Senha incorreta! Tentativas restantes: " + tentativasRestantes);
+                                                                        } else {
+                                                                                    alert("Acesso bloqueado! Número máximo de tentativas excedido.");
+                                                                                            }
+                                                                                                }
+
+                                                                                                } while (tentativasRestantes > 0);
