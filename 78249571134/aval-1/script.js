e10a78e11fe69f30f4e40f028837fc41967cba8a
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-04-28 13:34:47 -0300

    publish(78249571134): 1

---
+++ 78249571134/aval-1/script.js
@@ -0,0 +1,13 @@
+const botao = document.getElementById("btnColor");
+
+botao.addEventListener("click", () => {
+
+    document.body.style.transition = "0.5s";
+
+        document.body.style.background =
+            "linear-gradient(135deg, #ffd4e5, #fff0f7)";
+
+                botao.innerHTML = "Tendência carregada 💅";
+
+                    alert("As unhas mais estilosas de 2026 acabaram de aparecer!");
+                    });
