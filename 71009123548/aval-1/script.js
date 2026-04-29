45c72955c2c5476d136b1430e114b03e8fcbf0ca
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-04-29 14:33:41 -0300

    publish(71009123548): 1

---
+++ 71009123548/aval-1/script.js
@@ -0,0 +1,14 @@
+/* Mensagem de boas-vindas */
+window.onload = function() {
+    alert("Bem-vindo ao site de Turismo Ambiental em Guaribas!");
+}
+
+/* Função para mostrar mais informações */
+function mostrarMensagem() {
+    alert("Guaribas é um ótimo lugar para ecoturismo e preservação ambiental!");
+}
+
+/* Mudar cor do fundo */
+function mudarCor() {
+    document.body.style.backgroundColor = "#e6ffe6";
+}
