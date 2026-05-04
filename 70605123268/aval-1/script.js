425fbd5d1289166557ec7be0bbe9341ce5b43d45
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-03 22:45:46 -0300

    publish(70605123268): 1

---
+++ 70605123268/aval-1/script.js
@@ -0,0 +1,24 @@
+let senhaCorreta = "6767";
+let tentativas = 3;
+
+function verificar() {
+  let senha = document.getElementById("senha").value;
+    let msg = document.getElementById("msg");
+
+      if (tentativas <= 0) {
+          msg.innerHTML = "🚫 Acesso bloqueado!";
+              return;
+                }
+
+                  if (senha === senhaCorreta) {
+                      msg.innerHTML = "✅ Acesso liberado! Bem-vindo!";
+                        } else {
+                            tentativas--;
+
+                                if (tentativas > 0) {
+                                      msg.innerHTML = "❌ Senha incorreta! Tentativas restantes: " + tentativas;
+                                          } else {
+                                                msg.innerHTML = "🚫 Acesso bloqueado!";
+                                                    }
+                                                      }
+                                                      }
