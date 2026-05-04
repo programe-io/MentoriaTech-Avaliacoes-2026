78efee9b73566b922ea0c55563e83140678c81eb
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-04 10:38:19 -0300

    publish(70311364195): 2

---
+++ 70311364195/aval-2/script.js
@@ -0,0 +1,11 @@
+function abrirImagem(card) {
+    const img = card.querySelector("img");
+
+    // abre a imagem em nova aba
+    window.open(img.src, "_blank");
+}function abrirImagem(card) {
+    const img = card.querySelector("img");
+
+    // abre a imagem em nova aba
+    window.open(img.src, "_blank");
+}
