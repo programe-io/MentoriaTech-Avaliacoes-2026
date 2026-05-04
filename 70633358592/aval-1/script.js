5a611fd99f798544ca2fb6db8878d1674a119e96
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-04 14:46:34 -0300

    publish(70633358592): 1

---
+++ 70633358592/aval-1/script.js
@@ -0,0 +1,15 @@
+// Informações do estudante
+const nome = "Josivane Araujo";
+const curso = "Desenvolvimento de Sistemas";
+const escola = "CETI Francisca Trindade";
+
+// Função que exibe uma mensagem
+function mostrarMensagem() {
+    alert(`Olá! Meu nome é ${nome}.
+Sou estudante do curso de ${curso}
+na escola ${escola}.`);
+}
+
+// Mostrar no console também
+console.log("Perfil carregado com sucesso!");
+console.log(nome, curso, escola);
