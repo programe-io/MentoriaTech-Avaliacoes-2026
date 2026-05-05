ef24e11541d3cfc7d666cf4b71e62e34fc9ffd83
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-05 14:02:19 -0300

    publish(79614984716): 1

---
+++ 79614984716/aval-1/script.js
@@ -0,0 +1,6 @@
+function mudarTexto() {
+    const titulo = document.getElementById("titulo");
+    
+    titulo.textContent = "Você clicou no botão!";
+    document.body.style.backgroundColor = "#cce5ff";
+}
