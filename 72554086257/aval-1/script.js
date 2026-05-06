56a6afaa83c6b711cf935551f95f961af7c71b58
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-06 15:02:16 -0300

    publish(72554086257): 1

---
+++ 72554086257/aval-1/script.js
@@ -0,0 +1,10 @@
+function mostrarMais() {
+        const conteudo = document.getElementById("conteudo-extra");
+
+            if (conteudo.classList.contains("hidden")) {
+                    conteudo.classList.remove("hidden");
+                        } else {
+                                conteudo.classList.add("hidden");
+                                    }
+                                    }
+}
