a045d9325c5916bffcf1db40249efa16837c7a24
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-08 07:44:57 -0300

    publish(70024039195): 1

---
+++ 70024039195/aval-1/script.js
@@ -0,0 +1,9 @@
+// Mensagem ao carregar a página
+window.onload = function () {
+  alert("Seu ambiente de envio de códigos foi carregado com sucesso!");
+  };
+
+  // Função de confirmação de envio
+  function enviarCodigo() {
+    alert("Código enviado para correção!");
+    }
