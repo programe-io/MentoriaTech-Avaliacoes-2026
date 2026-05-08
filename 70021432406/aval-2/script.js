554c6ff45b04cd85881757919e095bb8d8292dde
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-08 07:52:58 -0300

    publish(70021432406): 2

---
+++ 70021432406/aval-2/script.js
@@ -0,0 +1,26 @@
+// Seleciona o botão
+const botao = document.querySelector("button");
+
+// Contador de cliques
+let cliques = 0;
+
+// Evento de clique
+botao.addEventListener("click", function () {
+  cliques++;
+
+  // Mostra quantidade de cliques
+  botao.innerText = `Cliques: ${cliques}`;
+
+  // Muda a cor do botão aleatoriamente
+  const cores = ["#ff4757", "#1e90ff", "#2ed573", "#ffa502", "#a55eea"];
+
+  const corAleatoria =
+    cores[Math.floor(Math.random() * cores.length)];
+
+  botao.style.background = corAleatoria;
+});
+
+// Mensagem ao carregar a página
+window.onload = function () {
+  console.log("Página carregada com sucesso!");
+};
