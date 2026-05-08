21751c98ae64f7b8c2a1c25c10c0397a4a0aaa82
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-08 07:59:34 -0300

    publish(70024039195): 3

---
+++ 70024039195/aval-3/script.js
@@ -0,0 +1,7 @@
+const titulo = document.querySelector("h1");
+
+titulo.addEventListener("click", function() {
+    titulo.innerText = "Você clicou no título!";
+    });
+
+    alert("Bem-vindo ao site!");
