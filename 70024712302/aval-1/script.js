4e000c5036ac76bf4eb72d5d510778300e954409
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-08 10:59:02 -0300

    publish(70024712302): 1

---
+++ 70024712302/aval-1/script.js
@@ -0,0 +1,19 @@
+// Função chamada ao clicar no botão
+function mostrarMensagem() {
+  alert("Olá! JavaScript funcionando com sucesso!");
+  }
+
+  // Muda a cor do fundo automaticamente
+  function mudarCor() {
+    const cores = ["#f4f4f4", "#dff6ff", "#ffe4e1", "#e8ffd7"];
+      
+        const corAleatoria = cores[Math.floor(Math.random() * cores.length)];
+          
+            document.body.style.background = corAleatoria;
+            }
+
+            // Executa a função a cada 3 segundos
+            setInterval(mudarCor, 3000);
+
+            // Mostra mensagem no console
+            console.log("Arquivo JavaScript carregado!");
