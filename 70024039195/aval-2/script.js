2f071c94c097d0751667c4fcfa2694df6e7c5c38
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-08 07:55:54 -0300

    publish(70024039195): 2

---
+++ 70024039195/aval-2/script.js
@@ -0,0 +1,7 @@
+// Seleciona o botão
+const botao = document.querySelector("button");
+
+// Adiciona evento de clique
+botao.addEventListener("click", function () {
+  alert("Código enviado para correção com sucesso!");
+  });
