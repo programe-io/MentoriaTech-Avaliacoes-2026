3cb0561be19a4f78e2c4808b7d1fb947597075b3
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-12 13:41:26 -0300

    publish(80325775249): 3

---
+++ 80325775249/aval-3/script.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("botao");
+
+botao.addEventListener("click", function(){
+
+  document.body.style.background = "#d6eaf8";
+
+  alert("Tema alterado com sucesso!");
+
+});
