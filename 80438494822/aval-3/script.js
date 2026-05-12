c12dbe487b1243bf76083b895268467148a9b452
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-12 08:56:35 -0300

    publish(80438494822): 3

---
+++ 80438494822/aval-3/script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("botao");
+const texto = document.getElementById("texto");
+
+botao.addEventListener("click", function() {
+  texto.textContent =
+    "HTML permite criar a estrutura básica de qualquer site na internet.";
+});
