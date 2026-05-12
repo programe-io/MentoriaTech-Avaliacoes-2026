7aea87e83e5f40bc2d942743c4db377cf1ba458e
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-12 09:57:18 -0300

    publish(79896936358): 3

---
+++ 79896936358/aval-3/script.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+
+    mensagem.textContent =
+        "Mais notícias serão exibidas em breve!";
+
+});
