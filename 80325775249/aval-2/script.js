8a97015f1477e736c65b988e579f479846aa7ba0
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-12 13:38:38 -0300

    publish(80325775249): 2

---
+++ 80325775249/aval-2/script.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("botao");
+
+botao.addEventListener("click", function(){
+
+  document.body.style.background = "#dbeafe";
+
+  alert("JavaScript funcionando com sucesso!");
+
+});
