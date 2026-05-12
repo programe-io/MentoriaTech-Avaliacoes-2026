d749cff8c71c5c6761ed7cb06df15c721f3c9b99
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-12 12:15:54 -0300

    publish(71253503842): 1

---
+++ 71253503842/aval-1/style.css
@@ -0,0 +1,15 @@
+const senha = '123456'
+let tentativas = 3;
+
+do{
+    let confirmasenha = prompt('informe sua senha: ');
+    
+    if(confirmasenha === senha){
+        console.log('acesso permitido!!!');
+    }
+    else{
+        tentativas--; 
+        console.log(`senha incoreta. restam '${tentativas}'tentativas. `);
+    }
+    
+    }while (tentativas > 0);
