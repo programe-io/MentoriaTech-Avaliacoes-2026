e73404cb30f7ee5c4a09c3a99e3c483b302582e2
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-12 08:44:27 -0300

    publish(80175484112): 3

---
+++ 80175484112/aval-3/script.js
@@ -0,0 +1,10 @@
+// script.js
+
+function mostrarMais() {
+
+  const texto = document.getElementById("texto");
+
+  texto.innerHTML =
+    "Empresas estão investindo cada vez mais em inteligência artificial. Novas ferramentas prometem transformar áreas como educação, saúde e desenvolvimento web.";
+
+}
