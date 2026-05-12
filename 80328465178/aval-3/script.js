db79a33ddb6eba927ec95330d2c3f96609aa62a3
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-12 08:52:28 -0300

    publish(80328465178): 3

---
+++ 80328465178/aval-3/script.js
@@ -0,0 +1,9 @@
+vconst botao = document.getElementById("btn");
+const texto = document.getElementById("texto");
+
+botao.addEventListener("click", () => {
+
+    texto.innerHTML =
+        "Obrigado por visitar o Portal Tech!";
+
+});
