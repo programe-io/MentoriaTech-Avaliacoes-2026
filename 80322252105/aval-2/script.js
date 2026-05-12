b92f46ec8b2f400561d33271b3cca0730082a9d6
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-12 08:35:58 -0300

    publish(80322252105): 2

---
+++ 80322252105/aval-2/script.js
@@ -0,0 +1,10 @@
+// Seleciona o botão pelo ID
+const botao = document.getElementById("btn");
+
+// Adiciona evento de clique
+botao.addEventListener("click", function() {
+
+  // Exibe mensagem
+  alert("Projeto funcionando corretamente!");
+
+});
