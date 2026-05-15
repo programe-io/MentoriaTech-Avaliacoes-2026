9f031ca4ceb10ca895c6ab2eb1c92d6402b626de
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-15 13:33:30 -0300

    publish(71736224379): 1

---
+++ 71736224379/aval-1/script.js
@@ -0,0 +1,1 @@
+#JAVASCRIPT // script.js const button = document.getElementById("btnMensagem"); const mensagem = document.getElementById("mensagem"); button.addEventListener("click", () => { mensagem.innerText = "Você clicou no botão!"; }); 
