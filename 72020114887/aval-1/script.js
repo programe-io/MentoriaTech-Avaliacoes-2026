40aa8268f1f76de8f95e561b55838a2cee36fbfe
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-15 07:58:10 -0300

    publish(72020114887): 1

---
+++ 72020114887/aval-1/script.js
@@ -0,0 +1,8 @@
+let nome = "João Victor";
+let idade = 16;
+
+let cidade = "Floresta do Piauí";
+
+console.log("Nome:", nome); // Exibe: Nome: João Victor
+console.log("Idade:", idade); // Exibe: Idade: 16
+console.log("Cidade:", cidade); // Exibe: Cidade: Floresta do Piauí
