d85e9edfc2466e3a9439e9084de295e2e9e49e7a
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-15 08:00:33 -0300

    publish(73321070359): 1

---
+++ 73321070359/aval-1/script.js
@@ -0,0 +1,7 @@
+let nome = "Francisco Junior";
+let idade = 16;
+let cidade = "Floresta do Piauí";
+
+console.log("Nome:", nome);
+console.log("Idade:", idade, "anos");
+console.log("Cidade:", cidade);
