bc58fb9018ffdb3c1eb5927b88f82ee2c0e040d5
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-15 08:29:28 -0300

    publish(72012506240): 1

---
+++ 72012506240/aval-1/script.js
@@ -0,0 +1,11 @@
+// Criando objeto do usuário
+let usuario = {
+    nome: "Levi",
+    idade: 16,
+    cidade: "Floresta do Piauí"
+};
+
+// Apresentando as informações na tela
+console.log("Nome: " + usuario.nome);
+console.log("Idade: " + usuario.idade);
+console.log("Cidade: " + usuario.cidade);
