938fb3a72f50c3e54057ec63d9b6b4c212ead90c
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-15 07:59:22 -0300

    publish(71367914256): 1

---
+++ 71367914256/aval-1/script.js
@@ -0,0 +1,16 @@
+5// Criando objeto do usuário
+let usuario = {
+    nome: "Maria",
+    idade: 22,
+    cidade: "Rio de Janeiro",
+    profissao: "Designer"
+};
+
+// Mostrando na tela
+document.getElementById("resultado").innerHTML =
+    "Nome: " + usuario.nome + "<br>" +
+    "Idade: " + usuario.idade + "<br>" +
+    "Cidade: " + usuario.cidade + "<br>" +
+    "Profissão: " + usuario.profissao;
+
+    
