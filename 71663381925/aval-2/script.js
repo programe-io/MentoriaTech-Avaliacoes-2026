8d6e1b36406968cfd63042e21d33725a542a652b
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-15 14:49:59 -0300

    publish(71663381925): 2

---
+++ 71663381925/aval-2/script.js
@@ -0,0 +1,9 @@
+// script.js
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+            alert("Abrindo artigo...");
+                });
+                });
+                
