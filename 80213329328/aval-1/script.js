57fb8b17aea508dc6f53521950978ac21827f6bf
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-19 16:03:34 -0300

    publish(80213329328): 1

---
+++ 80213329328/aval-1/script.js
@@ -0,0 +1,4 @@
+// Este código roda automaticamente quando a página é carregada
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Página da Ana Letícia carregada com sucesso! 🚀");
+});
