6ce25f9999d1cbbbd208751cfe2cbc1159aae26b
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-20 15:30:43 -0300

    publish(34447448270): 2

---
+++ 34447448270/aval-2/script.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("btnMensagem");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function(){
+
+    mensagem.innerHTML =
+    "Cristiano Ronaldo é inspiração para milhões de pessoas! ⚽🔥";
+
+});
