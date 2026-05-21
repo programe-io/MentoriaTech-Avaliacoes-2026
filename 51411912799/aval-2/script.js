a5c33f71f2497a18c5a9ddc52bbf42c1ce636f6e
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-20 22:05:40 -0300

    publish(51411912799): 2

---
+++ 51411912799/aval-2/script.js
@@ -0,0 +1,11 @@
+// BOTÃO
+
+const botao = document.getElementById("botao")
+
+botao.addEventListener("click", function(){
+
+    botao.innerHTML = "Entrou no vlog 🎵"
+
+        alert("Bem-vinda ao vlog da Camila!")
+
+        })
