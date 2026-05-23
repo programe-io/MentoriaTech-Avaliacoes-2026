f94fbaba300bc6ebbc001613ef76c1de5a2f7262
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-23 20:04:10 -0300

    publish(62053311576): 1

---
+++ 62053311576/aval-1/script.js
@@ -0,0 +1,13 @@
+// PEGANDO TODOS OS BOTÕES
+const botoes = document.querySelectorAll(".btn");
+
+// FUNÇÃO PARA CADA BOTÃO
+botoes.forEach(function(botao){
+
+    botao.addEventListener("click", function(){
+
+            alert("Você clicou para saber mais sobre essa série turca 💖");
+
+                });
+
+                });
