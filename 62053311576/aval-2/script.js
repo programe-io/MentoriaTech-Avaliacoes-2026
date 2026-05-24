38c4cbf2d4834969cb6f14bf7400c5b8ca24b007
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-24 19:06:46 -0300

    publish(62053311576): 2

---
+++ 62053311576/aval-2/script.js
@@ -0,0 +1,20 @@
+// PEGANDO TODOS OS BOTÕES
+const botoes = document.querySelectorAll(".btn");
+
+// FUNÇÃO PARA CADA BOTÃO
+botoes.forEach(function(botao){
+
+    botao.addEventListener("click", function(){
+
+            alert("Oi! Eu sou Ana Lívia 💖 Obrigada por visitar meu vlog de séries turcas!");
+
+                });
+
+                });
+
+                // MENSAGEM AO ABRIR O SITE
+                window.addEventListener("load", function(){
+
+                    console.log("Vlog da Ana Lívia carregado com sucesso!");
+
+                    });
