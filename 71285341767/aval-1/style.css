d3100fd43a91feb5693afb5ed2b1d5c17de23e77
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-26 14:13:36 -0300

    publish(71285341767): 1

---
+++ 71285341767/aval-1/style.css
@@ -0,0 +1,9 @@
+let indiceAtual = Number(prompt('Informe o índice de poluição atual: '));
+
+if (indiceAtual < 35) {
+    console.log('Agradável');
+} else if (indiceAtual >= 35 && indiceAtual <= 60) {
+    console.log('Desagradável');
+} else {
+    console.log('Perigoso');
+}
