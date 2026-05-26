3d4c949acecd21673142535afd901ee52dbcd607
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-26 12:07:10 -0300

    publish(72300221110): 1

--- 72300221110/aval-1/script.js
+++ 72300221110/aval-1/script.js
@@ -1,1 +1,13 @@
-https://www.programiz.com/javascript/online-compiler/
+let indiceAtual = Number(prompt('Informe o indice de poluição atual'))
+
+if(indiceAtual >= 35){
+    console.log('Agradavel');
+    }
+
+if(indiceAtual >= 35 && indiceAtual <=60){
+    console.log('dessagradavel')
+}
+
+else{
+    console.log('perigoso');
+}
