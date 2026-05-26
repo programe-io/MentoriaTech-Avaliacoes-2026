8b5802a6972a91a2eda0562aacbea2295250fb92
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-26 15:54:57 -0300

    publish(71006394370): 1

---
+++ 71006394370/aval-1/script.js
@@ -0,0 +1,8 @@
+function mostrarCuriosidade(){
+
+  const curiosidade = document.getElementById("curiosidade");
+
+  curiosidade.innerHTML =
+  "As serras de Guaribas fazem parte de uma região importante para a preservação da Caatinga, único bioma exclusivamente brasileiro. A área abriga espécies raras e ameaçadas de extinção.";
+
+}
