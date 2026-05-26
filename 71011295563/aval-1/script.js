70bb2f122ca76f39c45f04b6131f7333f9716a57
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-26 15:24:25 -0300

    publish(71011295563): 1

---
+++ 71011295563/aval-1/script.js
@@ -0,0 +1,9 @@
+// Função para o botão do banner principal
+function mensagemBoasVindas() {
+    alert("✈️ Ótima escolha! Role a página e clique no botão 'Quero Conhecer' do destino desejado.");
+}
+
+// Função para o clique nos botões de cada destino
+function solicitarOrcamento(nomeDestino) {
+    alert(`🗺️ Destino: ${nomeDestino}\n\nSua solicitação de interesse foi registrada com sucesso! Entraremos em contato com os melhores pacotes promocionais.`);
+}
