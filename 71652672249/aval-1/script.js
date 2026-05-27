55de07c721c3c4c8b4a9e1d5b6dc033d1ea71922
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-05-27 10:44:14 -0300

    publish(71652672249): 1

---
+++ 71652672249/aval-1/script.js
@@ -0,0 +1,22 @@
+    <div class="card">
+      <h2>JavaScript</h2>
+      <p>Interatividade da página.</p>
+      <button onclick="mostrarMensagem('Você clicou em JavaScript!')">
+        Clique Aqui
+      </button>
+    </div>
+
+  </section>
+
+  <footer>
+    <p>© 2026 - Página criada por Verônica</p>
+  </footer>
+
+  <script>
+    function mostrarMensagem(texto){
+      alert(texto);
+    }
+  </script>
+
+</body>
+</html>
