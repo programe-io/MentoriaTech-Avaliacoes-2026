4fdd8fd1fbced8105ad0a7a496b066c7bc5123b1
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-01 10:21:36 -0300

    publish(57728160699): 1

---
+++ 57728160699/aval-1/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+alert("Seja bem-vindo(a) ao meu site!");
+
+function mostrarMensagem() {
+    alert("Olá! Meu nome é Mizilene Lima, tenho 15 anos e estudo no 2º ano do Ensino Médio.");
+}
