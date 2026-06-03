914f2b6d06cdf4c69d206546154098282a62e800
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-03 11:44:17 -0300

    publish(73577031620): 1

---
+++ 73577031620/aval-1/script.js
@@ -0,0 +1,20 @@
+// Exibe uma mensagem no console
+console.log("Olá, mundo!");
+
+// Declaração de variáveis
+let nome = "Letícia";
+let idade = 16;
+
+// Exibe informações
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+
+// Função para somar dois números
+function somar(a, b) {
+    return a + b;
+\}
+
+// Chamada da função
+let resultado = somar(10, 5);
+
+console.log("Resultado da soma: " + result$0
