c1f589596e56c32852f2d691f42257308db714f0
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-03 14:02:01 -0300

    publish(51475225873): 2

---
+++ 51475225873/aval-2/script.js
@@ -0,0 +1,8 @@
+alert("Bem-vindo ao site do Brocasito!");
+
+const titulo = document.getElementById("titulo");
+
+titulo.addEventListener("click", function () {
+    titulo.innerText = "Brocasito 🔥";
+        titulo.style.color = "#00ffcc";
+        });
