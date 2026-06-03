e04fb33bc2ebe80e5dd595eaeffa6142b8fb5c0d
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-03 13:49:53 -0300

    publish(49741999828): 2

---
+++ 49741999828/aval-2/script.js
@@ -0,0 +1,4 @@
+function mostrarCuriosidade() {
+    document.getElementById("curiosidade").innerHTML =
+        "Neymar começou sua carreira profissional no Santos em 2009 e ficou famoso muito jovem.";
+}
