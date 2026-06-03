2eb1229d09479112dff191e4644d2b376f74ae05
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-03 08:04:59 -0300

    publish(74395418163): 2

---
+++ 74395418163/aval-2/script.js
@@ -0,0 +1,10 @@
+/**
+ * Função simples para testar a conexão do arquivo JS com o HTML
+ */
+function mensagem() {
+    // Exibe um alerta na tela
+    alert('Olá, Professor! O arquivo script.js foi carregado e executado com sucesso.');
+    
+    // Log interno no console do navegador (F12) para boas práticas
+    console.log('O botão de teste foi clicado com sucesso.');
+}
