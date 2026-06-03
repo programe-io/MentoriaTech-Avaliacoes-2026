b65bd49c0ba8c928c52af40f599f94c7ff956da4
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-03 08:10:04 -0300

    publish(71225336236): 1

---
+++ 71225336236/aval-1/script.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Página Completa</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+
+    <div class="card">
+        <h1 id="titulo">Bem-vindo ao meu site!</h1>
+        <p>Clique no botão abaixo para ver o JavaScript em ação mudando as coisas em tempo real.</p>
+        
+        <button id="meuBotao" class="botao">Clique Aqui</button>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
