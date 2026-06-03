6780e4626a299f4fcb1bbff28527d60d408e6a40
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-03 08:24:41 -0300

    publish(71225336236): 2

---
+++ 71225336236/aval-2/script.js
@@ -0,0 +1,8 @@
+const nome = "Mariana"; // String (texto)
+let idade = 28;         // Number (número)
+const programadora = true; // Boolean (verdadeiro ou falso)
+
+// Modificando um valor let
+idade = 29; 
+
+console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos.`);
