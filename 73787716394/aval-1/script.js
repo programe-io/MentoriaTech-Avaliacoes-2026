d66f39b6d9dc1af999c16e525a329ed6c8e47292
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-03 11:26:57 -0300

    publish(73787716394): 1

---
+++ 73787716394/aval-1/script.js
@@ -0,0 +1,14 @@
+function mudarCor() {
+    const cores = [
+        "#ffadad",
+        "#ffd6a5",
+        "#fdffb6",
+        "#caffbf",
+        "#9bf6ff",
+        "#a0c4ff"
+    ];
+
+    const corAleatoria = cores[Math.floor(Math.random() * cores.length)];
+
+    document.body.style.backgroundColor = corAleatoria;
+\}$0
