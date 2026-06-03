01cc3f360f5bdc24e5a6cb3dd93e6050fd7a6ce4
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-03 13:55:30 -0300

    publish(49741999828): 3

---
+++ 49741999828/aval-3/script.js
@@ -0,0 +1,8 @@
+function mostrarCuriosidade() {
+    document.getElementById("curiosidade").innerHTML =
+        "O voleibol foi criado em 1895 por William G. Morgan.";
+}
+
+function mostrarMensagem() {
+    alert("Voleibol: esporte de equipe, reflexo rápido e paciência com saque ruim.");
+}
