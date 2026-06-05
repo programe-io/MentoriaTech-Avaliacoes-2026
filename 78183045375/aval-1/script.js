be76071907561344c418343900c8733cc26c48e6
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-05 14:40:01 -0300

    publish(78183045375): 1

--- 78183045375/aval-1/script.js
+++ 78183045375/aval-1/script.js
@@ -1,1 +1,8 @@
-console.log("Olá, mundo!");
+function curtir(botao) {
+        let likes = botao.nextElementSibling;
+            let quantidade = parseInt(likes.textContent);
+
+                quantidade++;
+                    likes.textContent = quantidade + " curtidas";
+                    }
+}
