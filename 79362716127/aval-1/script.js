f0a89777eb6b6850e014ff0a3ced87d1db20b5d3
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-04 22:23:56 -0300

    publish(79362716127): 1

---
+++ 79362716127/aval-1/script.js
@@ -0,0 +1,10 @@
+<script>
+// ===== CONFIG MECHA =====
+const MECHA_CONFIG = {
+    sounds: true, // muda pra false se não quiser som
+        bootSequence: true,
+            glitchEffect: true,
+                hudBeep: true
+                };
+
+                // ===== SONS MECHA -
