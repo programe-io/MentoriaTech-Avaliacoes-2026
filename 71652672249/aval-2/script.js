e524d53f68ba24918d364a39e4d5f07e996e64f4
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-08 14:52:48 -0300

    publish(71652672249): 2

---
+++ 71652672249/aval-2/script.js
@@ -0,0 +1,6 @@
+function saudar(nome) {
+    return "Olá, " + nome + "! Bem-vindo de volta.";
+}
+
+// Chamando a função:
+console.log(saudar("Carlos")); // Resultado: Olá, Carlos! Bem-vindo de volta.
