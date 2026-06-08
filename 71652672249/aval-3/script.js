3ea4f564d74065a0cde3fd5c2d2fb8b9afe8b011
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-08 14:58:07 -0300

    publish(71652672249): 3

---
+++ 71652672249/aval-3/script.js
@@ -0,0 +1,3 @@
+let nomeUsuario = "Ana"; // Variável que pode ter o valor alterado
+const pi = 3.1415;       // Constante, o valor permanece fixo
+let estaLogado = true;   // Booleano (true ou false)
