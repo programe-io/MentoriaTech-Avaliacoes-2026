4b304e801b4e5733078689cc6b5798c7310784dc
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-08 12:09:32 -0300

    publish(28859784123): 1

---
+++ 28859784123/aval-1/script.js
@@ -0,0 +1,10 @@
+// Mensagem ao abrir a página
+window.onload = function () {
+    alert("Bem-vindo à página de apresentação do Heitor!");
+};
+
+// Função para mostrar informações
+function mostrarInfo() {
+    document.getElementById("info").innerHTML =
+        "Heitor, 15 anos, estudante do 1º Ano DSA. Futuro nutricionista ou médico!";
+}
