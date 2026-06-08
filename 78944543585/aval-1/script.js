3d6bd759f2a76557c3202cf0c6aed9b4c330c144
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-08 13:46:03 -0300

    publish(78944543585): 1

---
+++ 78944543585/aval-1/script.js
@@ -0,0 +1,73 @@
+ <!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pagina sobre mim</title>
+</head>
+<body>
+    
+     <header>
+
+     <h1>
+        um pouco sobre mim
+     </h1>
+
+     </header>
+
+     <main>
+       
+        <h2>ola eu sou a</h2>
+        <h3>maria vitoria</h3>
+
+         <h4>estudante de tec. em desenvolvimento de sistema</h4>
+
+         <p>
+           sou maria vitoria tenho 15 anos e meu hooby e assistir 
+           e decidir aprender tecnologia para fica mais rapida em 
+           algumas coisas
+         </p>
+
+        <section>
+
+          <h5>por encrivel que pareça nao uso tecnologia,alem do celular ,porem 
+            pretendo aprender mais pra fazer essas coisas de html, senction e head
+          </h5>
+
+        </section>
+
+        <section>
+
+       <h5>nao sou formadaporem oretendo mim formada
+        porem pretendo mim formar advogada
+       </h5>
+         
+       <ol>
+          
+        <li>tec.em desenvolvimento de sistemas</li>
+
+       </ol>
+
+        </section>
+           
+         
+
+        <section>
+
+        <h5>minhas redes socias</h5>
+         <a href="https://www.instagram.com/lov3y.vih/"><link></a>
+
+        </section>
+
+     </main>
+             <footer>
+
+            <p>copyringt 2026 - todos os direitos reservados - maria vitoria </p>
+
+             </footer>
+
+
+
+
+</body>
+</html>
