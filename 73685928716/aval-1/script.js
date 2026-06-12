833944ad904d015da85545b753addbab22153356
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-12 14:25:02 -0300

    publish(73685928716): 1

---
+++ 73685928716/aval-1/script.js
@@ -0,0 +1,24 @@
+function dark() {
+    document.body.classList.toggle("dark-mode");
+}
+
+function calcularArea() {
+    let inputBase = document.getElementById("base");
+    let inputAltura = document.getElementById("altura");
+    let resultado = document.getElementById("resultado");
+
+    let base = Number(inputBase.value);
+    let altura = Number(inputAltura.value);
+
+    if (isNaN(base) || isNaN(altura) || base <= 0 || altura <= 0) {
+        resultado.innerText = "Digite valores válidos";
+        return;
+    }
+
+    let area = base * altura;
+
+    resultado.innerText = "A área do retângulo é: " + area + " m²";
+
+    inputBase.value = "";
+    inputAltura.value = "";
+}
