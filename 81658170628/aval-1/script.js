9cc6e964b261bbe0170e4f22c826bdd758f1dae1
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-15 07:29:45 -0300

    publish(81658170628): 1

--- 81658170628/aval-1/script.js
+++ 81658170628/aval-1/script.js
@@ -1,31 +1,4 @@
-// Mensagem de boas-vindas
-window.onload = function () {
-    alert("🌸 Seja bem-vindo(a) ao Blog Estudantil da Lavinnya Macedo!");
-    };
-
-    // Mudar a cor de fundo
-    function mudarCor() {
-        const cores = [
-                "#ffe4ef",
-                        "#ffd6e8",
-                                "#fff0f5",
-                                        "#fce4ec",
-                                                "#f8bbd0"
-                                                    ];
-
-                                                        const cor = cores[Math.floor(Math.random() * cores.length)];
-                                                            document.body.style.backgroundColor = cor;
-                                                            }
-
-                                                            // Mostrar mensagem de incentivo
-                                                            function mostrarMensagem() {
-                                                                alert("💖 Nunca desista dos seus sonhos! Continue estudando e dando o seu melhor. 📚");
-                                                                }
-
-                                                                // Mostrar a data atual no rodapé
-                                                                const data = new Date();
-                                                                const rodape = document.querySelector("footer p");
-
-                                                                if (rodape) {
-                                                                    rodape.innerHTML += "<br>📅 " + data.toLocaleDateString("pt-BR");
-                                                                    }
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+        "Continue estudando e alcançando seus objetivos!";
+}
