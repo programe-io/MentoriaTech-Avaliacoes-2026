1821c2992df3f36b002941baf6a3be91bdedb4e6
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-15 14:34:37 -0300

    publish(72337874702): 1

---
+++ 72337874702/aval-1/script.js
@@ -0,0 +1,15 @@
+function darck(){
+    document.body.classList.toggle("dark-mode");
+}
+function calcularArea(){
+    let inputBase= document.getElementById("base");
+    let inputAltura= document.getElementById("altura");
+    let resultado= document.getElementById("resultado");
+
+let base=Number(inputBase.value);
+let altura =Number (inputAltura.value);
+if(isNaN(base)||isNaN(altura)){
+    resultado.innerText="digite valores válidos ";
+    return
+}
+let area=base*altura;
