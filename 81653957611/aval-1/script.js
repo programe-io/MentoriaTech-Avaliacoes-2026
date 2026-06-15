1932f83b2483fbe597baa6bacd4ae87baa32e032
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-15 08:37:57 -0300

    publish(81653957611): 1

---
+++ 81653957611/aval-1/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Blog carregado com sucesso!");
+});
+
+const titulo = document.querySelector("h1");
+
+titulo.addEventListener("click", () => {
+    alert("Bem-vindo ao meu blog!");
+});
