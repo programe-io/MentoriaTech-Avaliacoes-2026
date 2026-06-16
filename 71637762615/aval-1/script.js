f6cbbf7e184aca19c10869588f6b17e950c022e2
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-16 18:46:26 -0300

    publish(71637762615): 1

---
+++ 71637762615/aval-1/script.js
@@ -0,0 +1,12 @@
+function contar() {
+        let numero = 1;
+            let texto = "";
+
+                while (numero <= 10) {
+                        texto += numero + " ";
+                                numero++;
+                                    }
+
+                                        document.getElementById("resultado").innerHTML = texto;
+                                    
+}
