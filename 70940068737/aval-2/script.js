405ef12b1501036a18ac71190a449bbfc9dfb4d2
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-17 14:04:09 -0300

    publish(70940068737): 2

---
+++ 70940068737/aval-2/script.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").addEventListener("click", function(){
+
+    document.getElementById("curiosidade").textContent =
+    "Paris possui mais de 1.800 monumentos históricos e recebe milhões de turistas todos os anos.";
+
+});
