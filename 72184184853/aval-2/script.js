d552d9f6fa68bb7bd1dec645f6fd47d65a29f19c
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-17 11:03:34 -0300

    publish(72184184853): 2

---
+++ 72184184853/aval-2/script.js
@@ -0,0 +1,8 @@
+article img {
+        width: 100%;
+            max-width: 600px;
+                display: block;
+                    margin: 15px auto;
+                        border-radius: 8px;
+                        }
+}
