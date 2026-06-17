4740627eceee7d9fdf32f2da1146db779fb63815
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-17 13:43:40 -0300

    publish(70899555608): 1

---
+++ 70899555608/aval-1/script.js
@@ -0,0 +1,9 @@
+// Solicita o nome do usuário
+let nome = prompt("Qual é o seu nome?");
+
+// Exibe uma mensagem de boas-vindas
+if (nome) {
+    alert("Olá, " + nome + "! Seja bem-vindo!");
+} else {
+    alert("Você não informou seu nome.");
+}
