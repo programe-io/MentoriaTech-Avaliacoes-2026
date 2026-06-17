1d9173c329f129cda6ff907dca552bc132c3b13b
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-17 09:22:30 -0300

    publish(70671381520): 3

---
+++ 70671381520/aval-3/script.js
@@ -0,0 +1,10 @@
+let pontos = 0;
+
+function ganharPonto(){
+    pontos++;
+        document.getElementById("pontos").textContent = pontos;
+        }
+
+        function abrirJogo(){
+            alert("Bem-vindo ao Mini Game!");
+            }
