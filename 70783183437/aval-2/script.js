5f37bc9cf65072d612a572510364068123c41eaa
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-17 14:02:28 -0300

    publish(70783183437): 2

---
+++ 70783183437/aval-2/script.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").addEventListener("click", function(){
+
+    document.getElementById("textoExtra").textContent =
+    "A inteligência artificial continuará evoluindo e impactará áreas como saúde, educação, transporte e entretenimento.";
+
+});
