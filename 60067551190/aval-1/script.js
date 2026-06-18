c22573f1b28cf8225833761b3d62cb888b2b3aa6
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-18 09:17:10 -0300

    publish(60067551190): 1

---
+++ 60067551190/aval-1/script.js
@@ -0,0 +1,14 @@
+// Exibe uma mensagem no console
+console.log("Olá, mundo!");
+
+// Função para mostrar um alerta
+function mostrarMensagem() {
+    alert("Você clicou no botão!");
+}
+
+// Exemplo de cálculo
+let numero1 = 10;
+let numero2 = 5;
+let soma = numero1 + numero2;
+
+console.log("A soma é: " + soma);
