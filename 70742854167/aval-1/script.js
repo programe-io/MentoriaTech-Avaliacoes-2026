c221520976b81cb45aae7feaaf9e9cc3801adf01
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-18 09:33:50 -0300

    publish(70742854167): 1

---
+++ 70742854167/aval-1/script.js
@@ -0,0 +1,6 @@
+document.getElementById("btnFestival").addEventListener("click", function(){
+
+    document.getElementById("atracoes").textContent =
+    "Atrações principais: Banda Horizonte, DJ Night, Grupo Solar e convidados especiais.";
+
+});
