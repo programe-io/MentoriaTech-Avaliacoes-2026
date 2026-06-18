f77c838cc7ac70257f39346b7ee02aecc6003d17
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-18 09:30:41 -0300

    publish(70453356567): 3

---
+++ 70453356567/aval-3/script.js
@@ -0,0 +1,6 @@
+document.getElementById("btnNoticia").addEventListener("click", function(){
+
+    document.getElementById("destaque").textContent =
+    "O craque da partida marcou dois gols e foi eleito o melhor jogador em campo.";
+
+})
