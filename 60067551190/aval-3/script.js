98ec575351556a470e21a39b9e8534fcc5c5b138
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-18 09:44:21 -0300

    publish(60067551190): 3

---
+++ 60067551190/aval-3/script.js
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Mensagem no console
+        System.out.println("Olá, mundo!");
+
+        // Variáveis
+        int numero1 = 10;
+        int numero2 = 5;
+
+        int soma = numero1 + numero2;
+
+        System.out.println("A soma é: " + soma);
+    }
+}
