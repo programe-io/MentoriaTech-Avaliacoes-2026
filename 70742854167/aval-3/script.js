388fa9a1a862ff467b25b0afa9dba9744e6964eb
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-18 09:36:16 -0300

    publish(70742854167): 3

---
+++ 70742854167/aval-3/script.js
@@ -0,0 +1,6 @@
+document.getElementById("btnBTS").addEventListener("click", function(){
+
+    document.getElementById("curiosidade").textContent =
+    "O BTS tornou-se um dos grupos musicais mais populares do mundo, conquistando fãs em diversos países.";
+
+});
