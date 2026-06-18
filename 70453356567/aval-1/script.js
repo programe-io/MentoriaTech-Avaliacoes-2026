59fb56faca2ee6b460eb152544ec96d76238be43
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-18 09:18:58 -0300

    publish(70453356567): 1

---
+++ 70453356567/aval-1/script.js
@@ -0,0 +1,6 @@
+document.getElementById("btnReceita").addEventListener("click", function(){
+
+    document.getElementById("ingredientes").textContent =
+    "Ingredientes: 3 ovos, 2 xícaras de farinha, 1 xícara de chocolate em pó e 1 xícara de açúcar.";
+
+});
