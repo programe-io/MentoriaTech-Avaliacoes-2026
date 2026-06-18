71ad8ceaaea8dee916428e8e3966532a2abffaff
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-18 09:35:19 -0300

    publish(70742854167): 2

---
+++ 70742854167/aval-2/script.js
@@ -0,0 +1,6 @@
+document.getElementById("btnVeigh").addEventListener("click", function(){
+
+    document.getElementById("curiosidade").textContent =
+    "Veigh se destacou no cenário do trap nacional e conquistou milhões de reproduções em suas músicas.";
+
+});
