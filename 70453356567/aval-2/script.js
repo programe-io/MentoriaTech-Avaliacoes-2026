563407b0976059eedba8c4ad45397120022cd571
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-18 09:27:03 -0300

    publish(70453356567): 2

---
+++ 70453356567/aval-2/script.js
@@ -0,0 +1,6 @@
+document.getElementById("btnPizza").addEventListener("click", function(){
+
+    document.getElementById("promocao").textContent =
+    "Promoção do dia: Compre uma pizza grande e ganhe um refrigerante grátis!";
+
+});
