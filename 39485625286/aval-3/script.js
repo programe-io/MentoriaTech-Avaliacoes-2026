7b9a5cd79ebda792fe63b55a93d907305cda40c1
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-18 09:30:58 -0300

    publish(39485625286): 3

---
+++ 39485625286/aval-3/script.js
@@ -0,0 +1,7 @@
+// Seleciona o botão
+const botao = document.getElementById("meuBotao");
+
+// Adiciona um evento de clique
+botao.addEventListener("click", function() {
+    alert("Olá! Você clicou no botão.");
+});
