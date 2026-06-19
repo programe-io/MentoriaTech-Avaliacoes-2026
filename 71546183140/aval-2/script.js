beae47e68d5c0bdb9837b87b9c321ac9394f2c32
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-19 16:27:12 -0300

    publish(71546183140): 2

---
+++ 71546183140/aval-2/script.js
@@ -0,0 +1,6 @@
+function mostrarCuriosidade() {
+
+    document.getElementById("curiosidade").innerHTML =
+        "⚽ Curiosidade: Yan Diomande é reconhecido pela velocidade, habilidade no drible e capacidade de criar jogadas ofensivas em situações de 1 contra 1.";
+
+}
