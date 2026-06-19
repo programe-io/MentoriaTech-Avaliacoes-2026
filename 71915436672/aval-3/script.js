d7dd0a548879e7ed5d20d810cd6b140f7e545418
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-19 16:40:48 -0300

    publish(71915436672): 3

---
+++ 71915436672/aval-3/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const titulo = document.querySelector("h1");
+
+    titulo.addEventListener("click", () => {
+        alert("Bem-vindo ao Portfólio de Carros!");
+    });
+
+});
