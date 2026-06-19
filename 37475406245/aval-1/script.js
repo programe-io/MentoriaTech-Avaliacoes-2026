3a92f4ef36c7db22aa8ab4274cf6c895ae58699b
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-19 13:27:02 -0300

    publish(37475406245): 1

---
+++ 37475406245/aval-1/script.js
@@ -0,0 +1,6 @@
+ <script>
+        function mostrarMensagem() {
+            document.getElementById("mensagem").innerHTML =
+                "Olá, Aissa Lorrany! Seja bem-vinda ao seu site! ✨";
+        }
+    </script>
