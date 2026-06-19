f988b90282071d83de65b43ab9818353c52f9709
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-19 16:34:23 -0300

    publish(71915436672): 2

---
+++ 71915436672/aval-2/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const titulo = document.querySelector("header h1");
+
+    titulo.addEventListener("click", () => {
+        alert("Bem-vindo ao Portfólio de Motos!");
+    });
+
+});
