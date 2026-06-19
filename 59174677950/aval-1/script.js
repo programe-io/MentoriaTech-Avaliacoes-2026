2af946d98e351209e59aa91c8c0044913eb5ec08
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-19 16:37:50 -0300

    publish(59174677950): 1

---
+++ 59174677950/aval-1/script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click", function () {
+    document.getElementById("msg").innerText =
+        "🎊 Presença confirmada! Te esperamos no arraiá!";
+});
