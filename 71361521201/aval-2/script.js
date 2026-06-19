f2094ab509f4fec1589e8369889e28aaa6f1d433
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-19 09:24:31 -0300

    publish(71361521201): 2

---
+++ 71361521201/aval-2/script.js
@@ -0,0 +1,14 @@
+// Declarando uma variável
+let mensagem = "Olá, mundo!";
+
+// Exibindo a mensagem no console do navegador
+console.log(mensagem);
+
+// Verificando uma condição simples
+let hora = 12;
+if (hora < 12) {
+    console.log("Bom dia!");
+} else {
+    console.log("Boa tarde!");
+}
+
