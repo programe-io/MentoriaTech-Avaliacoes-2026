bb2b2ff5fcd7ed5ba420a32c9a98bbbe1d3e9027
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-19 08:15:55 -0300

    publish(71016093888): 1

---
+++ 71016093888/aval-1/script.js
@@ -0,0 +1,9 @@
+// Printing plain text
+console.log("Hello, World!"); 
+
+// Printing variables
+let username = "Alex";
+console.log(username); 
+
+// Printing multiple values together
+console.log("User:", username, "Status: Active"); 
