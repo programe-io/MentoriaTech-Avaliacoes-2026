7b12ecf9ab95b1aeef3c5270591a85deb67fbf9d
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-19 15:43:58 -0300

    publish(71962077381): 1

---
+++ 71962077381/aval-1/script.js
@@ -0,0 +1,8 @@
+function comprar(carro){
+    alert("Você selecionou o modelo: " + carro +
+          "\nNossa equipe entrará em contato!");
+}
+
+function mostrarMensagem(){
+    alert("Solicitação de orçamento enviada com sucesso!");
+}
