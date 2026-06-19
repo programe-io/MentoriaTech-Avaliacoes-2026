c40bbea4c308c35e2a798bb6516425d1dae21824
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-19 15:51:04 -0300

    publish(71546183140): 1

---
+++ 71546183140/aval-1/script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() {
+
+    document.getElementById("mensagem").innerHTML =
+    "Olá! Eu sou David Pereira dos Santos Neto. Tenho 16 anos, sou estudante do 2º Ano do Ensino Médio, cursando Desenvolvimento de Sistemas, bolsista e atleta de futsal. Obrigado por visitar meu portfólio! 🚀⚽";
+
+}
