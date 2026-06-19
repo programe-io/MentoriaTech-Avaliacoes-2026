2bc3d4bb05cc4640a94f59763016fb0a32e65992
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-19 14:36:45 -0300

    publish(48372866899): 3

---
+++ 48372866899/aval-3/script.js
@@ -0,0 +1,9 @@
+function mostrar() {
+    alert("Botão clicado!");
+}
+
+console.log("JavaScript funcionando!");
+
+document.querySelector("h1").addEventListener("click", function() {
+    alert("Título clicado!");
+});
