f13daa1e3b47f31cbe162e7fef78c5b3297ef924
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-22 15:31:51 -0300

    publish(72066653691): 2

---
+++ 72066653691/aval-2/script.js
@@ -0,0 +1,4 @@
+function mostrarPromocao() {
+    document.getElementById("resultado").innerHTML =
+    "🔥 Oferta Relâmpago: Revisão Completa + Troca de Óleo + Lavagem Grátis por apenas R$ 199,90!";
+}
