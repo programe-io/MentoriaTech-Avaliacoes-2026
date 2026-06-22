6ce9c617713ef81a3c25bf0597c33a9ccf2b2ab8
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-22 15:24:59 -0300

    publish(71550318489): 1

---
+++ 71550318489/aval-1/script.js
@@ -0,0 +1,27 @@
+public class CR7Historia {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== História de Cristiano Ronaldo ===");
+
+        System.out.println("Nome: Cristiano Ronaldo dos Santos Aveiro");
+        System.out.println("Nascimento: 05/02/1985");
+        System.out.println("País: Portugal");
+
+        System.out.println("\nClubes:");
+        System.out.println("- Sporting CP");
+        System.out.println("- Manchester United");
+        System.out.println("- Real Madrid");
+        System.out.println("- Juventus");
+        System.out.println("- Manchester United");
+        System.out.println("- Al Nassr");
+
+        System.out.println("\nConquistas:");
+        System.out.println("- 5 Bolas de Ouro");
+        System.out.println("- 5 Champions League");
+        System.out.println("- Eurocopa 2016");
+        System.out.println("- Diversos títulos nacionais");
+
+        System.out.println("\nCR7 é considerado um dos maiores jogadores da história do futebol.");
+    }
+}
