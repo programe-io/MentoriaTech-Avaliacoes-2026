5c59e70ca70de971e27de2af3c812f15141497b7
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-22 15:37:43 -0300

    publish(71550318489): 3

---
+++ 71550318489/aval-3/script.js
@@ -0,0 +1,25 @@
+public class KierkegaardHistoria {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== HISTÓRIA DE SØREN KIERKEGAARD ===\n");
+
+        System.out.println("Nome: Søren Aabye Kierkegaard");
+        System.out.println("Nascimento: 5 de maio de 1813");
+        System.out.println("Local: Copenhague, Dinamarca\n");
+
+        System.out.println("PROFISSÃO:");
+        System.out.println("- Filósofo");
+        System.out.println("- Teólogo");
+        System.out.println("- Escritor\n");
+
+        System.out.println("PRINCIPAIS OBRAS:");
+        System.out.println("- Temor e Tremor");
+        System.out.println("- Ou Isto ou Aquilo");
+        System.out.println("- O Desespero Humano\n");
+
+        System.out.println("LEGADO:");
+        System.out.println("Kierkegaard é considerado o pai do existencialismo.");
+        System.out.println("Suas ideias influenciaram a filosofia moderna.");
+    }
+}
