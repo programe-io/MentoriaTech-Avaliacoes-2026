7e6c3da59abd9e3162cc220479ec10349d9805c2
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-22 15:30:41 -0300

    publish(71550318489): 2

---
+++ 71550318489/aval-2/script.js
@@ -0,0 +1,27 @@
+public class MbappeHistoria {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== HISTÓRIA DE KYLIAN MBAPPÉ ===\n");
+
+        System.out.println("Nome completo: Kylian Mbappé Lottin");
+        System.out.println("Nascimento: 20 de dezembro de 1998");
+        System.out.println("País: França\n");
+
+        System.out.println("CARREIRA:");
+        System.out.println("- Início nas categorias de base francesas");
+        System.out.println("- Estreia profissional pelo AS Monaco");
+        System.out.println("- Destaque no futebol europeu");
+        System.out.println("- Transferência para o Paris Saint-Germain (PSG)");
+        System.out.println("- Capitão da Seleção Francesa");
+        System.out.println("- Transferência para o Real Madrid\n");
+
+        System.out.println("CONQUISTAS:");
+        System.out.println("- Copa do Mundo FIFA 2018");
+        System.out.println("- Diversos títulos franceses");
+        System.out.println("- Artilharias nacionais e internacionais");
+        System.out.println("- Reconhecimento como um dos melhores jogadores do mundo\n");
+
+        System.out.println("Mbappé é conhecido por sua velocidade, dribles e capacidade de marcar gols decisivos.");
+    }
+}
