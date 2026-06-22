b9534b49bf99cfda47252bd0b24035a25e9ac320
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-22 13:20:31 -0300

    publish(78583542414): 2

---
+++ 78583542414/aval-2/script.js
@@ -0,0 +1,10 @@
+function mostrarMensagem() {
+    alert(
+        "Curiosidade: Messi conquistou a Copa do Mundo de 2022 e ganhou várias Bolas de Ouro durante sua carreira!"
+    );
+}
+
+// Efeito de boas-vindas
+window.onload = function() {
+    console.log("Bem-vindo ao site sobre Lionel Messi!");
+};
