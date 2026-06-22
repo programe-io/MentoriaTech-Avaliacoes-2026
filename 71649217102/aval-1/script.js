96b64f1632ebf8f324cd4d9ca0a01d2c05f84d28
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-22 09:35:47 -0300

    publish(71649217102): 1

---
+++ 71649217102/aval-1/script.js
@@ -0,0 +1,17 @@
+// Mensagem quando abrir o site
+window.onload = function () {
+    alert("Bem-vindo ao meu blog!");
+    };
+
+    // Mudar cor do título ao clicar
+    const titulo = document.querySelector("header");
+
+    titulo.addEventListener("click", function () {
+
+        if (titulo.style.background === "purple") {
+                titulo.style.background = "#2196f3";
+                    } else {
+                            titulo.style.background = "purple";
+                                }
+
+                                });
