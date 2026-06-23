10f69ff544261456b5bc2244e1d689a87fcd85b9
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-23 14:29:14 -0300

    publish(71908777230): 3

---
+++ 71908777230/aval-3/script.js
@@ -0,0 +1,7 @@
+function mostrarMensagem(){
+
+    const texto = document.getElementById("mensagem");
+
+    texto.innerHTML = "🍕 Nossa especialidade é a Pizza, preparada com ingredientes frescos e muito sabor!";
+
+}
