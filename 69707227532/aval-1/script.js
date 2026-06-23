90e1b22155718327850c6ac2084afa951936904d
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-23 12:10:04 -0300

    publish(69707227532): 1

---
+++ 69707227532/aval-1/script.js
@@ -0,0 +1,10 @@
+// Mensagem de boas-vindas
+window.onload = function () {
+    alert("Bem-vindo ao meu projeto Sobre Mim!");
+};
+
+// Função para mostrar uma mensagem
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+        "Obrigado por visitar meu projeto!";
+}
