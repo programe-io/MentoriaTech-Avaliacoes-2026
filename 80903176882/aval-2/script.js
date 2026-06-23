417995022e2952d78fdc92e69880cf110d3a71b4
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-23 14:13:50 -0300

    publish(80903176882): 2

---
+++ 80903176882/aval-2/script.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Bike JS Store</title>
+</head>
+<body>
+
+  <h1>🚲 Bike JS Store</h1>
+
+  <div id="app"></div>
+
+  <h2>🛒 Carrinho</h2>
+  <ul id="carrinho"></ul>
+
+  <script src="script.js"></script>
+</body>
+</html>
