a35f0a73ef3a13cf7e4d0866da86e7525f9e3072
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-23 08:03:38 -0300

    publish(80950822124): 1

---
+++ 80950822124/aval-1/script.js
@@ -0,0 +1,19 @@
+// Variáveis
+let nome = "mayara";
+let idade = 15;
+
+// Exibir mensagem
+console.log("Olá, " + nome);
+console.log("Você tem " + idade + " anos");
+
+// Função
+function mostrarMensagem() {
+    alert("Bem-vindo ao meu site!");
+}
+
+// Cálculo
+let numero1 = 10;
+let numero2 = 5;
+let resultado = numero1 + numero2;
+
+console.log("Resultado: " + resultado);
