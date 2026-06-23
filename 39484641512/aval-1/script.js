836d8fd0250ddd570bea96ccbaa999c23aef64e5
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-23 13:49:13 -0300

    publish(39484641512): 1

---
+++ 39484641512/aval-1/script.js
@@ -0,0 +1,11 @@
+let contador = 0;
+
+function contarCliques() {
+    contador++;
+    document.getElementById("resultado").textContent =
+        "Você clicou " + contador + " vez(es).";
+}
+
+function mostrarMensagem() {
+    alert("Olá! Bem-vindo ao JavaScript.");
+}
