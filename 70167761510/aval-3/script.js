011d227a47ee6d0045e93def93b8234f2b57ac94
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-24 13:24:15 -0300

    publish(70167761510): 3

---
+++ 70167761510/aval-3/script.js
@@ -0,0 +1,9 @@
+// Cria o morcego
+const morcego = document.createElement("div");
+morcego.textContent = "🦇";
+morcego.style.fontSize = "120px";
+morcego.style.textAlign = "center";
+morcego.style.marginTop = "100px";
+
+// Adiciona na página
+document.body.appendChild(morcego);
