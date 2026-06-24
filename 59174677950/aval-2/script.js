01c45874a5bac17375a95ed5ff3e5d295b26ef83
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-24 13:39:46 -0300

    publish(59174677950): 2

---
+++ 59174677950/aval-2/script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+    "O racismo é crime. A escola deve ser um espaço de respeito, igualdade e inclusão para todos.";
+}
