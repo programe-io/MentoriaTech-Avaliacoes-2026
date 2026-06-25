3defe64ba3658cdf1e0ff0a3a497ecfd34ba99c7
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-25 11:46:00 -0300

    publish(48365323419): 1

---
+++ 48365323419/aval-1/script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("botao");
+
+botao.addEventListener("click", () => {
+    const cores = ["#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff"];
+    const cor = cores[Math.floor(Math.random() * cores.length)];
+    document.body.style.backgroundColor = cor;
+});
