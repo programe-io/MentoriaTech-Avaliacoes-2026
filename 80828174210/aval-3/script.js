0da60bcd462a6bf9eb4796e0269daab60e06e4d1
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-25 09:47:47 -0300

    publish(80828174210): 3

---
+++ 80828174210/aval-3/script.js
@@ -0,0 +1,15 @@
+function resenha(){
+
+    document.getElementById("resenha").innerHTML =
+    "⭐ Uma obra emocionante sobre amizade, sonhos e descobertas.";
+
+}
+
+
+
+function capitulo(){
+
+    document.getElementById("capitulo").innerHTML =
+    "🦊 O encontro com a raposa é um dos momentos mais importantes do livro.";
+
+}
