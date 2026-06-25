3cdfea0158bd35c21327bed5f0cd7b029f5da906
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-25 11:07:07 -0300

    publish(73827444110): 1

---
+++ 73827444110/aval-1/script.js
@@ -0,0 +1,37 @@
+
+man[
+flex: 70%;
+backgrond-color:#wite;
+padding:20px;
+
+
+
+
+.fakeimg[
+
+backgroubd-color:#aaaaa;
+widih:100%
+padding:28px;
+margin-botton 10px;
+
+
+footer
+padding:30px;
+text-align:conter;
+backegroud-#100e17;
+color:#fffff;
+
+
+
+
+
+[media screen and [max-width:700px}[
+nav[
+    flex-directio:colun;
+]
+
+.row[
+
+    flex-directio:colun;
+]
+]
