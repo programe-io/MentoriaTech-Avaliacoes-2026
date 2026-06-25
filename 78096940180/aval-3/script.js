4a8548f7627b4671915d35792442980258940eb7
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-25 09:35:25 -0300

    publish(78096940180): 3

---
+++ 78096940180/aval-3/script.js
@@ -0,0 +1,13 @@
+function curtirPost(botao) {
+  botao.innerText = "Curtido ❤️";
+  botao.style.background = "#a29bfe";
+}
+
+function enviarMensagem(event) {
+  event.preventDefault();
+
+  let nome = document.getElementById("nome").value;
+  let resposta = document.getElementById("resposta");
+
+  resposta.innerText = `Obrigado pela mensagem, ${nome}! 🎵 Entraremos em contato em breve.`;
+}
