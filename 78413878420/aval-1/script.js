e6905c3c64788e8bfc809f0cb652cd607747ef83
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-26 14:56:41 -0300

    publish(78413878420): 1

--- 78413878420/aval-1/script.js
+++ 78413878420/aval-1/script.js
@@ -1,4 +1,8 @@
-function mostrarMensagem() {
-    document.getElementById("mensagem").innerHTML =
-        "Parabéns! A partir de agora você é um six seven.";
-}
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function(){
+
+mensagem.innerHTML =
+"🌸 Yasmin, nunca deixe que o medo seja maior que os seus sonhos. Continue estudando, acreditando em você e dando um passo de cada vez. Um dia você vestirá o jaleco com orgulho e realizará o sonho de ser uma grande cirurgiã plástica. Eu acredito em você! 💖👩🏻‍⚕️✨";
+
+});
