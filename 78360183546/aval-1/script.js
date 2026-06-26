dd60d48ee35bc7b9851028db681db5130fe7ec70
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-26 14:10:23 -0300

    publish(78360183546): 1

---
+++ 78360183546/aval-1/script.js
@@ -0,0 +1,11 @@
+// Seleciona o botão
+const botao = document.getElementById("botao");
+
+// Executa quando o botão é clicado
+botao.addEventListener("click", function(){
+
+    alert(
+            "🌹 Curiosidade:\n\nExistem mais de 30 mil variedades de rosas cultivadas no mundo!"
+                );
+
+                });
