1f3d7f39d1c816f84777676673d203a866b61ef8
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-26 13:48:55 -0300

    publish(78447484394): 1

---
+++ 78447484394/aval-1/script.js
@@ -0,0 +1,9 @@
+// Seleciona todos os botões da página
+const botoes = document.querySelectorAll(".btn");
+
+// Adiciona a função de clique em cada botão
+botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+            alert("Você curtiu esse post!");
+                });
+                });
