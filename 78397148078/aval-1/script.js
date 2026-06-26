01af111a24f9f3e3e1de932efe1a66ae921c59cb
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-26 11:55:35 -0300

    publish(78397148078): 1

---
+++ 78397148078/aval-1/script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("btnMensagem");
+
+botao.addEventListener("click", () => {
+  alert(
+    "Yuri Alberto é um dos principais atacantes do Corinthians e se destaca pela velocidade e pelos gols!"
+  );
+});
