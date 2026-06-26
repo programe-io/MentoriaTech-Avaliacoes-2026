ef6ea719bb8707a5ddef1066c6751e9412b074b2
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-26 13:45:33 -0300

    publish(78213492447): 1

---
+++ 78213492447/aval-1/script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem(){
+
+    document.getElementById("mensagem").innerHTML =
+    "Jair Bolsonaro foi presidente do Brasil de 2019 a 2022. Este blog apresenta informações básicas sobre sua trajetória política.";
+}
