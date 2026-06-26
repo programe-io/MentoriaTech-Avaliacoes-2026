8c5e7736f78519c711f14d539c3a6612419297d8
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-26 11:33:47 -0300

    publish(78312103031): 1

---
+++ 78312103031/aval-1/script.js
@@ -0,0 +1,9 @@
+function mostrarMensagem(){
+
+    alert("🔥 Jhenyfer é linda! 🔥");
+
+    document.getElementById("mensagem").innerHTML =
+    "😎 Jhenyfer é zika dmss!<br><br>" +
+    "💪 Jhenyfer é foda!<br><br>" +
+    "🖤🤍 Jhenyfer ama o Corinthians! 🦅";
+}
