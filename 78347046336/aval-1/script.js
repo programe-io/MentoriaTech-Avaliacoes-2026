862067b1de50bac07f749315e04e788503718ef5
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-26 14:11:43 -0300

    publish(78347046336): 1

---
+++ 78347046336/aval-1/script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+    "⚽ Memphis Depay é conhecido por sua técnica, criatividade e por representar a seleção dos Países Baixos em competições internacionais.";
+}
