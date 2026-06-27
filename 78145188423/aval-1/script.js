cfcc749c54f3e28c3797d7f3e313e590f52ccd96
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-27 11:23:44 -0300

    publish(78145188423): 1

---
+++ 78145188423/aval-1/script.js
@@ -0,0 +1,32 @@
+function fl(){
+    window.location.href="https://docs.google.com/document/d/1es_TOH1GwJcvR0R1vAkqU_KUdHCmK7uM/edit?usp=sharing&ouid=102983485858705190260&rtpof=true&sd=true"
+}
+
+function ia(){
+    window.location.href="https://docs.google.com/document/d/1Lj6CYGijiTYrq8FivF1Gd1tjK2PQRGMw/edit?usp=sharing&ouid=102983485858705190260&rtpof=true&sd=true"
+}
+
+function si(){
+    window.location.href="https://docs.google.com/document/d/1XMqbIUl57dWUzn8OJKijNQbCgF18vB0d/edit?usp=sharing&ouid=102983485858705190260&rtpof=true&sd=true"
+}
+
+
+function t10(){
+    window.location.href="https://docs.google.com/document/d/1I2aysx3jV7AfWuHIsgPT1jZjK7rukhKHnyRVBsOPDUE/edit?usp=sharing"
+}
+
+function fire(){
+    window.location.href="https://docs.google.com/document/d/1TS7-68bhEIwGtw6QkNzVbm8szzmCBr5shxoDEXV_Mog/edit?usp=sharing"
+}
+
+function secrets(){
+    window.location.href="https://docs.google.com/document/d/1UO4zLiSAoK9Z2fUmTKPm7uskFwjFSDQGDPF8jS8NPMo/edit?usp=sharing"
+}
+
+function red(){
+    window.location.href="https://docs.google.com/document/d/1UFobQU1-2ssIUFUqCP1c16R-Lgnb_FPHThsuLdZwPKw/edit?usp=sharing"
+}
+
+function girl(){
+    window.location.href="https://docs.google.com/document/d/1Jj0lhgwFSY6Ad6q8C7Twb5r9UYtS4X8n9zvNMJKXgg4/edit?usp=sharing"
+}
