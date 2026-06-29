f37308ce973a2b1c196eadb557c6542c4ad90a70
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-29 10:20:41 -0300

    publish(75691138570): 3

---
+++ 75691138570/aval-3/script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+    "O autismo faz parte da diversidade humana. Respeito, inclusão e compreensão são fundamentais para uma sociedade melhor.";
+}
