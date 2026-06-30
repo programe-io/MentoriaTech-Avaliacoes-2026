579b840e343c03949fb87329dc69b497021206b8
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-06-30 14:40:21 -0300

    publish(70297246437): 1

---
+++ 70297246437/aval-1/script.js
@@ -0,0 +1,7 @@
+let nome = "José";
+console.log(`Oi, meu nome é ${nome}.`);
+
+let cidade = "Teresina";
+console.log(`Moro em ${cidade}.`);
+
+console.log("Oi, meu nome é José e esse é meu primeiro código em JavaScript.");
