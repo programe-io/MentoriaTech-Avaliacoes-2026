59cee220517e0878e4f0cb55cb4db52c828c4271
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-01 11:19:12 -0300

    publish(70281387249): 2

---
+++ 70281387249/aval-2/script.js
@@ -0,0 +1,12 @@
+// O console.log exigido na sua instrução
+console.log("Olá, mundo!");
+
+// Definindo a idade
+const idade = 25;
+
+// Exibindo a idade também no console, se quiser conferir
+console.log("Idade:", idade);
+
+// Selecionando os elementos do HTML para mostrar os dados na página de forma bonita
+document.getElementById("mensagem").textContent = "Olá, mundo!";
+document.getElementById("exibir-idade").textContent = `Idade: ${idade} anos`;
