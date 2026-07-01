f4ca142bd3e7bf57b35d80076a9b7f3a1942f3d7
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-01 19:36:41 -0300

    publish(71951243404): 3

---
+++ 71951243404/aval-3/script.js
@@ -0,0 +1,6 @@
+function mostrar(){
+
+        document.getElementById("texto").innerHTML =
+            "🚗 O Dodge Charger de Dominic Toretto é um dos carros mais famosos da história do cinema.";
+            }
+}
