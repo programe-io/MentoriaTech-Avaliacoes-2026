df3069a46431ba119be4a13da433a1112311e3f0
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-02 09:24:50 -0300

    publish(64239722519): 1

--- 64239722519/aval-1/script.js
+++ 64239722519/aval-1/script.js
@@ -1,3 +1,18 @@
-function mostrarMensagem() {
-    alert("Olá! Você clicou no botão.");
+const imagens = [
+    "https://picsum.photos/id/1015/600/350",
+    "https://picsum.photos/id/1025/600/350",
+    "https://picsum.photos/id/1043/600/350",
+    "https://picsum.photos/id/1069/600/350"
+];
+
+let indice = 0;
+
+function trocarImagem() {
+    indice++;
+
+    if (indice >= imagens.length) {
+        indice = 0;
+    }
+
+    document.getElementById("imagem").src = imagens[indice];
 }
