145925f9fa70c7958e10be7afd5b11ff6d248398
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-02 14:13:58 -0300

    publish(72075329583): 2

---
+++ 72075329583/aval-2/script.js
@@ -0,0 +1,5 @@
+const botao = document.getElementById("btnMensagem");
+
+botao.addEventListener("click", function () {
+    alert("🔥 Neymar Jr. é um dos maiores craques da história do futebol brasileiro!");
+});
