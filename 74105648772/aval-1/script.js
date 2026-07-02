b6f0f5d1e520b963a4786294e6954d3011f07fd6
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-02 13:12:12 -0300

    publish(74105648772): 1

---
+++ 74105648772/aval-1/script.js
@@ -0,0 +1,16 @@
+// Sobre Mim
+
+let nome = "Miguel";
+let idade = 16;
+let cidade = "Teresina";
+let curso = "Jogos Digitais";
+let hobby = "Jogar videogame";
+
+console.log("=== SOBRE MIM ===");
+console.log("Nome: " + nome);
+console.log("Idade: " + idade + " anos");
+console.log("Cidade: " + cidade);
+console.log("Curso: " + curso);
+console.log("Hobby: " + hobby);
+
+console.log("Olá! Meu nome é " + nome + ", tenho " + idade + " anos e moro em " + cidade + ". Faço " + curso + " e gosto de " + hobby + ".");
