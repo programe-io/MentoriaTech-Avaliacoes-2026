7e621f64ffb62a41c5c72d7dd1f1568a0a3f7877
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-02 11:19:29 -0300

    publish(73992177890): 1

---
+++ 73992177890/aval-1/script.js
@@ -0,0 +1,4 @@
+function mostrarCuriosidade() {
+    document.getElementById("curiosidade").innerHTML =
+        "🏆 A Espanha venceu a Eurocopa 2008 e iniciou uma das maiores eras da história do futebol, conquistando também a Copa do Mundo de 2010 e a Eurocopa de 2012.";
+}
