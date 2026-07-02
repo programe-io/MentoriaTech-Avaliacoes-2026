476e14349ff44851f8ae216652ffd6dc9682d6c1
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-02 09:44:54 -0300

    publish(77466313752): 1

--- 77466313752/aval-1/script.js
+++ 77466313752/aval-1/script.js
@@ -1,3 +1,32 @@
-function mudarTexto() {
-    document.getElementById("titulo").innerHTML = "Texto alterado com JavaScript!";
-}
+const botao = document.getElementById("modo");
+
+botao.addEventListener("click", () => {
+    document.body.classList.toggle("claro");
+
+    if(document.body.classList.contains("claro")){
+        botao.textContent="🌞";
+    }else{
+        botao.textContent="🌙";
+    }
+});
+
+const pesquisa = document.getElementById("pesquisa");
+const cards = document.querySelectorAll(".card");
+
+pesquisa.addEventListener("keyup", () => {
+
+    let texto = pesquisa.value.toLowerCase();
+
+    cards.forEach(card => {
+
+        let titulo = card.querySelector("h2").textContent.toLowerCase();
+
+        if(titulo.indexOf(texto) > -1){
+            card.style.display="block";
+        }else{
+            card.style.display="none";
+        }
+
+    });
+
+});
