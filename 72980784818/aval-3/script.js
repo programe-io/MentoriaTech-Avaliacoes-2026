247783ffa3be477b1d5a2080ea45b2e81bcb1544
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-02 14:19:49 -0300

    publish(72980784818): 3

---
+++ 72980784818/aval-3/script.js
@@ -0,0 +1,9 @@
+// Mensagem de boas-vindas
+window.onload = function () {
+    alert("Bem-vindo à MotoMax!");
+};
+
+// Função para exibir detalhes da moto
+function verDetalhes(nomeMoto) {
+    alert("Você selecionou: " + nomeMoto);
+}
