02a45a0c3602aae6801802d372ac02b817af5982
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-03 10:44:47 -0300

    publish(82629743390): 3

---
+++ 82629743390/aval-3/script.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelectorAll("button");
+
+botoes.forEach(botao => {
+
+botao.addEventListener("click", ()=>{
+
+alert("Sua matrícula foi solicitada com sucesso!");
+
+});
+
+});
