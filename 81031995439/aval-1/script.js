f4849e96a9d4b558a0356045e2b013207f49ebae
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-03 08:37:52 -0300

    publish(81031995439): 1

--- 81031995439/aval-1/script.js
+++ 81031995439/aval-1/script.js
@@ -1,41 +1,9 @@
-function adicionarTarefa(){
+const bloco = document.getElementById("grama");
 
-    let campo = document.getElementById("tarefa");
-    let texto = campo.value.trim();
+bloco.addEventListener("click", () => {
+    bloco.style.transform = "scale(0.9)";
 
-    if(texto === ""){
-        alert("Digite uma tarefa.");
-        return;
-    }
-
-    let lista = document.getElementById("lista");
-
-    let item = document.createElement("li");
-
-    let span = document.createElement("span");
-    span.textContent = texto;
-
-    span.onclick = function(){
-        if(span.style.textDecoration == "line-through"){
-            span.style.textDecoration = "none";
-        }else{
-            span.style.textDecoration = "line-through";
-        }
-    };
-
-    let botao = document.createElement("button");
-    botao.textContent = "Excluir";
-    botao.className = "excluir";
-
-    botao.onclick = function(){
-        lista.removeChild(item);
-    };
-
-    item.appendChild(span);
-    item.appendChild(botao);
-
-    lista.appendChild(item);
-
-    campo.value = "";
-    campo.focus();
-}
+    setTimeout(() => {
+        bloco.style.transform = "scale(1)";
+    }, 100);
+});
