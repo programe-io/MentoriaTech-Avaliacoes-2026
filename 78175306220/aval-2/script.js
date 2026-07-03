e5832e5da3afd355f3f12aa16ab4350d2c175fbd
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-03 10:46:48 -0300

    publish(78175306220): 2

---
+++ 78175306220/aval-2/script.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("btn");
+const resultado = document.getElementById("resultado");
+
+botao.addEventListener("click", function () {
+    resultado.innerHTML = "Parabéns! Seu código JavaScript está funcionando.";
+});
