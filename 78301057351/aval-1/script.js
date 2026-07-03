0089c31b26c2029435b00336b17e7ae6356038a7
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-03 11:03:43 -0300

    publish(78301057351): 1

---
+++ 78301057351/aval-1/script.js
@@ -0,0 +1,11 @@
+let contador = 0;
+
+function curtir() {
+
+    contador++;
+
+        document.getElementById("curtidas").innerHTML =
+                "Curtidas: " + contador;
+                }
+
+                
