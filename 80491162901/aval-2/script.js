4a7919d4bc23c7b860eb94e24238c20a08f9b60b
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-03 10:44:56 -0300

    publish(80491162901): 2

---
+++ 80491162901/aval-2/script.js
@@ -0,0 +1,11 @@
+<div class="card">
+    <img src="https://picsum.photos/150" alt="Perfil">
+
+    <h1>João Silva</h1>
+
+    <p>Desenvolvedor Front-end apaixonado por criar interfaces modernas e responsivas.</p>
+
+    <button id="botao">Saiba Mais</button>
+
+    <div id="mensagem"></div>
+</div>
