c443ba9cfea7345f5e3c1282fce2ebdc8ba419ee
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-03 10:15:36 -0300

    publish(78174115005): 1

---
+++ 78174115005/aval-1/script.js
@@ -0,0 +1,8 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function(){
+
+    mensagem.innerHTML = "🌍 Prepare as malas! Sua próxima aventura começa agora.";
+
+});
