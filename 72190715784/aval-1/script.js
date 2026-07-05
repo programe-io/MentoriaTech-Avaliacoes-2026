25567ce6017ad39090f8c012a3be5b5524321509
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-05 10:40:20 -0300

    publish(72190715784): 1

---
+++ 72190715784/aval-1/script.js
@@ -0,0 +1,11 @@
+<!-- Avaliação 1 - Página de apresentação -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Minha Avaliação 1</title>
+    </head>
+    <body>
+        <h1>Nome: Seu Nome</h1>
+            <p>Mentoria Tech - 2 Série</p>
+            </body>
+            </html>
