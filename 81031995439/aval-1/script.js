ed9529546007fdda6c3a2e99985791d92e436770
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-06 09:49:23 -0300

    publish(81031995439): 1

--- 81031995439/aval-1/script.js
+++ 81031995439/aval-1/script.js
@@ -1,9 +1,13 @@
 function mostrarInfo() {
   const info = document.getElementById("info");
 
-  if (info.style.display === "none" || info.style.display === "") {
-    info.style.display = "block";
-  } else {
+  if (info.classList.contains("show")) {
+    info.classList.remove("show");
     info.style.display = "none";
+  } else {
+    info.style.display = "block";
+    setTimeout(() => {
+      info.classList.add("show");
+    }, 10);
   }
 }
