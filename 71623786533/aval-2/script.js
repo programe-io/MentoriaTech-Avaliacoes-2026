a4343d2e1ec941aa514534a973eb9b0e6e5d5972
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-06 08:59:17 -0300

    publish(71623786533): 2

---
+++ 71623786533/aval-2/script.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("btnMensagem");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function(){
+
+    mensagem.innerHTML =
+    "🌸 Obrigada por visitar o Blog da Eloyse! Espero que você volte em breve! 💖";
+
+});
