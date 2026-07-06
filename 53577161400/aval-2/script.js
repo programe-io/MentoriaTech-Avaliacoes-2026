47349c9f8138d6c5a4f4b02ebb94535d1903577d
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-06 08:58:50 -0300

    publish(53577161400): 2

---
+++ 53577161400/aval-2/script.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function(){
+
+    mensagem.innerHTML =
+    "Obrigado por visitar minha página! Estou aprendendo programação e desenvolvendo novos projetos todos os dias.";
+
+});
