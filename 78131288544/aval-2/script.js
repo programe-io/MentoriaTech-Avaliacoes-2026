f77d6612ec0847e5050aa4fac8a761f81e427ac4
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-06 09:44:53 -0300

    publish(78131288544): 2

---
+++ 78131288544/aval-2/script.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", function(event){
+    event.preventDefault();
+
+    alert("Obrigado por visitar o site sobre Animais Selvagens!");
+});
