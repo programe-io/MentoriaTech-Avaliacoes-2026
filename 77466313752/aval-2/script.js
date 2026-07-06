3320f25320586fe82121878b3d2e5558687e25a7
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-06 08:51:50 -0300

    publish(77466313752): 2

--- 77466313752/aval-2/script.js
+++ 77466313752/aval-2/script.js
@@ -1,3 +1,34 @@
-function mudarTexto() {
-    document.getElementById("titulo").innerHTML = "Texto alterado com JavaScript!";
-}
+// Botão de modo escuro
+
+const tema = document.getElementById("tema");
+
+tema.addEventListener("click", function(){
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        tema.innerHTML = "☀️ Modo Claro";
+    }else{
+        tema.innerHTML = "🌙 Modo Escuro";
+    }
+
+});
+
+// Botões de curtidas
+
+const botoes = document.querySelectorAll(".curtir");
+
+botoes.forEach(function(botao){
+
+    let curtidas = 0;
+
+    botao.addEventListener("click", function(){
+
+        curtidas++;
+
+        botao.nextElementSibling.innerHTML =
+        "Curtidas: " + curtidas;
+
+    });
+
+});
