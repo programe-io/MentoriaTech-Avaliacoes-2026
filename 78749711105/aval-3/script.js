cbcac25130844c3cdf1051f3ca3c48d611411591
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-06 11:00:20 -0300

    publish(78749711105): 3

---
+++ 78749711105/aval-3/script.js
@@ -0,0 +1,6 @@
+function curiosidade(){
+
+    document.getElementById("mensagem").innerHTML =
+    "🥖 Você sabia? O pão existe há mais de 10 mil anos e é um dos alimentos mais consumidos do planeta.";
+
+}
