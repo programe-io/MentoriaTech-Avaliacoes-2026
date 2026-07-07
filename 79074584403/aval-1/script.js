6e05c5214892fe2a7cc29d8d09d4f2c8a36090d4
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-07 14:32:16 -0300

    publish(79074584403): 1

---
+++ 79074584403/aval-1/script.js
@@ -0,0 +1,5 @@
+const botao = document.getElementById("btnMensagem");
+
+botao.addEventListener("click", function () {
+    alert("Bem-vindo ao MeuBlog! Bons estudos de HTML, CSS e JavaScript.");
+});
