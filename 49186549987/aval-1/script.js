73e9e6a71cbe9b0e0084e1605b4eef5b34d6c4de
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-07 12:05:11 -0300

    publish(49186549987): 1

---
+++ 49186549987/aval-1/script.js
@@ -0,0 +1,14 @@
+// Mensagem ao abrir o site
+alert("Bem-vindo ao meu blog!");
+
+// Exibe uma mensagem no console
+console.log("Site carregado com sucesso!");
+
+// Mostra a data e a hora atuais
+const data = new Date();
+console.log("Data e hora:", data.toLocaleString());
+
+// Função para exibir uma mensagem
+function mostrarMensagem() {
+    alert("Obrigado por visitar o meu blog!");
+}
