43d0fa7b980a1e3890141dedf3666017e3d479d7
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-08 10:43:13 -0300

    publish(35992484671): 1

---
+++ 35992484671/aval-1/script.js
@@ -0,0 +1,14 @@
+```javascript
+function alterar() {
+
+    // Muda o texto do título
+        document.getElementById("titulo").innerHTML = "Texto alterado!";
+
+            // Muda o parágrafo
+                document.getElementById("texto").innerHTML = "O botão foi clicado com sucesso!";
+
+                    // Muda a cor do fundo
+                        document.body.style.backgroundColor = "#222";
+                        }
+                        ```
+                        
