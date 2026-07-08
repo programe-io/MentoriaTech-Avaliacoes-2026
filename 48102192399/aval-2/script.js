424dfe5c6ba4e377516249a9cae2ba7eef8ee896
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-08 11:01:26 -0300

    publish(48102192399): 2

---
+++ 48102192399/aval-2/script.js
@@ -0,0 +1,7 @@
+
+const botao = document.getElementById('botaoAlerta');
+
+
+botao.addEventListener('click', function() {
+    alert('Olá! Você clicou no botão que está dentro da tag <aside>!');
+    });
