345a746b148acf098c3d5bef576da6224543b288
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-28 10:42:03 -0300

    publish(40125817207): 2

--- 40125817207/aval-2/script.js
+++ 40125817207/aval-2/script.js
@@ -1,14 +1,5 @@
-const botao = document.getElementById("btnReceita");
-const preparo = document.getElementById("modoPreparo");
-
-botao.addEventListener("click", function(){
-
-    if(preparo.style.display === "block"){
-        preparo.style.display = "none";
-        botao.textContent = "Mostrar Modo de Preparo";
-    }else{
-        preparo.style.display = "block";
-        botao.textContent = "Ocultar Modo de Preparo";
-    }
-
-});
+function mostrarMensagem(){
+        document.getElementById("mensagem").innerHTML =
+            "Obrigada por visitar meu blog! 💙";
+            }
+}
