7f7bf7f14929ab41e8a5f1357a3721324fe6f5c8
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-07-28 10:31:31 -0300

    publish(40125817207): 1

--- 40125817207/aval-1/script.js
+++ 40125817207/aval-1/script.js
@@ -1,30 +1,5 @@
-// Mensagem de boas-vindas
-window.onload = function () {
-    alert("💙 Bem-vindo(a) ao blog da Maria Rita!");
-};
-
-// Mostrar a data e hora
-function mostrarData() {
-    const agora = new Date();
-
-    const opcoes = {
-        dateStyle: "full",
-        timeStyle: "short"
-    };
-
-    document.getElementById("data").innerHTML =
-        agora.toLocaleDateString("pt-BR", opcoes);
-}
-
-mostrarData();
-setInterval(mostrarData, 1000);
-
-// Botão para mudar a cor do tema
-function mudarTema() {
-    document.body.classList.toggle("escuro");
-}
-
-// Mensagem sobre os hobbies
-function mensagemHobby(hobby) {
-    alert("💙 Meu hobby favorito é: " + hobby);
+function mostrarMensagem() {
+        document.getElementById("mensagem").innerHTML =
+            "Obrigada por visitar meu blog! 💙 Espero que tenha gostado de conhecer um pouco sobre mim.";
+            }
 }
