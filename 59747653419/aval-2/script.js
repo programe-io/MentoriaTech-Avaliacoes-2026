4f79cce9e093df292c5b8cf5449cf4211f9bbd8d
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-03 14:47:56 -0300

    publish(59747653419): 2

---
+++ 59747653419/aval-2/script.js
@@ -0,0 +1,8 @@
+function mostrarMensagem(){
+
+let texto = document.getElementById("mensagem");
+
+texto.innerHTML =
+"Casos criminais devem ser analisados com responsabilidade, considerando os fatos, as provas e as decisões da Justiça.";
+
+}
