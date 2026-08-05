443cca45304f82f56acb5a2ad6b6ca2975f4f03a
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-05 20:58:28 -0300

    publish(70023622792): 1

--- 70023622792/aval-1/script.js
+++ 70023622792/aval-1/script.js
@@ -1,20 +1,5 @@
-// Mostrar mensagem
-function mostrarMensagem() {
-  alert("Olá! JavaScript funcionando 🚀");
-}
+const botao = document.querySelector("button");
 
-// Mudar cor do fundo
-function mudarCor() {
-  document.body.style.background = "#dff9fb";
-}
-
-// Mostrar horário atual
-function mostrarHora() {
-  const agora = new Date();
-  alert("Hora atual: " + agora.toLocaleTimeString());
-}
-
-// Exemplo de clique em botão
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Página carregada com sucesso!");
-});
+botao.addEventListener("click", function() {
+    alert("Olá! Você clicou no botão!");
+    });
