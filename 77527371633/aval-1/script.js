8599c076771835ab2782e933a566b67b29b3f0b4
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-06 16:00:49 -0300

    publish(77527371633): 1

---
+++ 77527371633/aval-1/script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem(){
+
+    document.getElementById("texto").innerHTML =
+    "💚 O CETI João Henrique de Almeida Sousa é motivo de orgulho para Teresina e para o Piauí. Com dedicação, compromisso e excelência, a escola forma estudantes preparados para os desafios da vida e para construir um futuro melhor.";
+
+}
