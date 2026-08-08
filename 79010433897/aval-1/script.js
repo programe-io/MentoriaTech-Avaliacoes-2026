edfb845ff5b7995573701c5928d4930e6431c9c3
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-07 22:56:45 -0300

    publish(79010433897): 1

---
+++ 79010433897/aval-1/script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() {
+
+    document.getElementById("mensagem").innerHTML =
+        "🧠📚 Ler regularmente pode ajudar a estimular a memória, a concentração e a criatividade!";
+
+}
