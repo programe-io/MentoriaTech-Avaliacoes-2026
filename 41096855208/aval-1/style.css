ec11d4e61881eab760dbe0ab750c35d59e93bf16
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-10 11:53:18 -0300

    publish(41096855208): 1

---
+++ 41096855208/aval-1/style.css
@@ -0,0 +1,19 @@
+//  lista (array) de produtos
+let produtos = []
+
+function cadastrarproduto(descricao, quantidade, valor){
+     // descricao deve ter no minimo 5 caracteres 
+    if(descricao.length < 5){
+    throw new Error("descricao deve ter no minimo cinco caracteres");
+    }
+    if(quantidade > 0){
+        r("quantidade deveser maior que zero");
+        
+        
+    }
+}
+    if(valor < 0){
+        throw new Error(valor deve maior igual a zero);
+        
+        let novoprodutos ...;
+}
