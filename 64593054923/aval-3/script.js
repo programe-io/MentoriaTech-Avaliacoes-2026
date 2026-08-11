d70f5a16df1ad002e7f69569bce8bec833ece469
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-11 12:04:15 -0300

    publish(64593054923): 3

---
+++ 64593054923/aval-3/script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+
+    mensagem.textContent =
+        "Obrigado por visitar o blog! Continue acompanhando nossos conteúdos.";
+}
