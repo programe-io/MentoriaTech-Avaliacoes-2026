bcfda769d5078f57c20bb562bf8b5c68a2a33552
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-12 08:42:03 -0300

    publish(79131899528): 1

---
+++ 79131899528/aval-1/script.js
@@ -0,0 +1,55 @@
+// ALTERAR O TEMA DO BLOG
+
+const botaoTema =
+    document.getElementById("botaoTema");
+
+
+botaoTema.addEventListener(
+    "click",
+    function() {
+
+        document.body.classList.toggle(
+            "modo-escuro"
+        );
+
+    }
+);
+
+
+
+// MOSTRAR UMA MENSAGEM
+
+const botaoMensagem =
+    document.getElementById(
+        "botaoMensagem"
+    );
+
+
+const mensagem =
+    document.getElementById(
+        "mensagem"
+    );
+
+
+botaoMensagem.addEventListener(
+    "click",
+    function() {
+
+        mensagem.textContent =
+            "Olá! Obrigada por visitar o blog da Ana Maynara!";
+
+    }
+);
+
+
+
+// COLOCAR O ANO ATUAL NO RODAPÉ
+
+const anoAtual =
+    document.getElementById(
+        "anoAtual"
+    );
+
+
+anoAtual.textContent =
+    new Date().getFullYear();
