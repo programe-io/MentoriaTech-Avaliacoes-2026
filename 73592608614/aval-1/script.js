8fb388d8f8d493db6827bdeeaaf982239d1a1310
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-17 15:37:44 -0300

    publish(73592608614): 1

---
+++ 73592608614/aval-1/script.js
@@ -0,0 +1,75 @@
+let produtos = [];
+
+function validarproduto(descricao, quantidade, valor) {
+    if (descricao.length < 5) {
+        throw new Error("Descrição deve ter no mínimo 5 caracteres");
+    }
+
+    if (quantidade < 1) {
+        throw new Error("Quantidade deve ser maior que zero");
+    }
+
+    if (valor < 0) {
+        throw new Error("Valor deve ser maior ou igual a zero");
+    }
+}
+
+function cadastrarproduto(descricao, quantidade, valor) {
+    validarproduto(descricao, quantidade, valor);
+
+    let novoproduto = {
+        codigo: produtos.length + 1,
+        descricao: descricao,
+        quantidade: quantidade,
+        valor: valor
+    };
+
+    produtos.push(novoproduto);
+}
+
+function listarprodutos() {
+    console.log(produtos);
+}
+
+function atualizarvalor(codigoproduto, novovalor) {
+    if (novovalor < 0) {
+        throw new Error("Valor deve ser maior ou igual a zero");
+    }
+
+    const produto = produtos.find(
+        prod => prod.codigo === codigoproduto
+    );
+
+    if (produto) {
+        produto.valor = novovalor;
+    } else {
+        throw new Error("Produto não encontrado");
+    }
+}
+
+function atualizarquantidade(codigoproduto, novaquantidade) {
+    if (novaquantidade < 1) {
+        throw new Error("Quantidade deve ser maior que zero");
+    }
+
+    const produto = produtos.find(
+        prod => prod.codigo === codigoproduto
+    );
+
+    if (produto) {
+        produto.quantidade = novaquantidade;
+    } else {
+        throw new Error("Produto não encontrado");
+    }
+}
+
+listarprodutos();
+
+cadastrarproduto("Bola de futebol", 51, 99.00);
+cadastrarproduto("Caneleira", 46, 78.00);
+
+listarprodutos();
+
+atualizarvalor(1,69.99);
+
+listarprodutos();
