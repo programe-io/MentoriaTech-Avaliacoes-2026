1680b54aec4ea9a47a312eab4736e3f82f833ed9
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-17 11:20:13 -0300

    publish(40611503980): 1

--- 40611503980/aval-1/script.js
+++ 40611503980/aval-1/script.js
@@ -1,3 +1,14 @@
 
-\\ lista [Array] de produtos
-let produtos = []
+// Lista (Array) de Produtos
+let produtos = [];
+
+function cadastrarProduto(descricao, quantidade, valor){
+    // Descricao deve ter no mínimo 5 caracteres
+    if(descricao.length < 5){
+        throw new Error("Descrição deve ter no mínimo 5 caracteres");
+    }
+    if(quantidade < 1){
+        throw new Error("Descrição deve ter no mínimo 5 caracteres");
+    }
+}
+```
