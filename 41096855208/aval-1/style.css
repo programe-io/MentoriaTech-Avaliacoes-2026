0098673b1ea207cb579640ae64aa763901432477
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-17 09:04:12 -0300

    publish(41096855208): 1

--- 41096855208/aval-1/style.css
+++ 41096855208/aval-1/style.css
@@ -1,27 +1,43 @@
 // Lista (array) de produtos
 let produtos = [];
 
-function cadastrarProduto(descricao, quantidade, valor) 
+// Função para cadastrar produto
+function cadastrarProduto(descricao, quantidade, valor) {
+
     // Descrição deve ter no mínimo 5 caracteres
     if (descricao.length < 5) {
         throw new Error("Descrição deve ter no mínimo cinco caracteres");
     }
+
     // Quantidade deve ser maior que zero
     if (quantidade <= 0) {
         throw new Error("Quantidade deve ser maior que zero");
     }
+
     // Valor deve ser maior ou igual a zero
     if (valor < 0) {
         throw new Error("Valor deve ser maior ou igual a zero");
-     }
-  { 
- 
- function cadastrarProduto(descricao, quantidade, valor)
-    validarproduto(descricao, quantidade, valor)
-    let novoproduto =  { 
-       "codigo";...,
-       "descrição"; descrição,
-        "quantidade";quantidade,
-         "valor";valor,
-          }
- }
+    }
+
+    // Gera um código para o produto
+    let codigo = produtos.length + 1;
+
+    // Cria o novo produto
+    let novoProduto = {
+        codigo: codigo.length + 1,
+        descricao: descricao,
+        quantidade: quantidade,
+        valor: valor
+    };
+      produto.spush(produtosnovo);
+}
+
+   function listarprodutos(){  
+     console.log(produtos);
+   }
+
+  function atualizarvalor(codigoproduto, novovalor){
+    in(novovalor > 0){
+
+    }
+  }
