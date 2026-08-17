69bfa9e5347afcdbeecb808db20b4499807b6874
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-17 11:23:41 -0300

    publish(59640387738): 1

---
+++ 59640387738/aval-1/script.js
@@ -0,0 +1,15 @@
+const botaoTema = document.getElementById("botaoTema");
+botaoTema.addEventListener("click", function() {
+    document.body.classList.toggle("modo-escuro");
+    });
+
+    const botaoMensagem = document.getElementById("botaoMensagem");
+    const mensagem = document.getElementById("mensagem");
+    botaoMensagem.addEventListener("click", function() {
+        mensagem.textContent = "Olá! Você está visitando o blog de Izaque Nicolas!";
+        });
+
+        const anoAtual = document.getElementById("anoAtual");
+        anoAtual.textContent = new Date().getFullYear();
+
+        
