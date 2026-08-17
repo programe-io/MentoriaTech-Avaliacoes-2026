3b9002f7cfc76138b83e6a37b2b9ee3f376f7e40
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-17 14:28:14 -0300

    publish(80867791411): 1

---
+++ 80867791411/aval-1/script.js
@@ -0,0 +1,13 @@
+const botaoTema = document.getElementById("botaoTema");
+botaoTema.addEventListener("click", function() {
+    document.body.classList.toggle("modo-escuro");
+});
+
+const botaoMensagem = document.getElementById("botaoMensagem");
+const mensagem = document.getElementById("mensagem");
+botaoMensagem.addEventListener("click", function() {
+    mensagem.textContent = "Olá! Você está visitando o blog de Isaias Teles!";
+});
+
+const anoAtual = document.getElementById("anoAtual");
+anoAtual.textContent = new Date().getFullYear();
