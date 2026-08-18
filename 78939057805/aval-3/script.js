740d44accbbd502b040cabf897cd878a3c815c59
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-18 16:11:20 -0300

    publish(78939057805): 3

---
+++ 78939057805/aval-3/script.js
@@ -0,0 +1,9 @@
+// Pegando o botão pelo ID
+const botao = document.getElementById("botaoMensagem");
+
+// Ação quando o botão for clicado
+botao.addEventListener("click", function () {
+
+    alert("🎮 Seja bem-vindo ao meu perfil, Mateus!");
+
+});
