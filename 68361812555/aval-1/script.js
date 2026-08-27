6ffeb982a08afff54b11e6c776181aebbfc37e35
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-27 09:31:47 -0300

    publish(68361812555): 1

--- 68361812555/aval-1/script.js
+++ 68361812555/aval-1/script.js
@@ -1,35 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <title>Contador</title>
-</head>
-<body>
+const botao = document.getElementById("botao");
 
-    <h1>Contador</h1>
-
-    <h2 id="numero">0</h2>
-
-    <button onclick="diminuir()">-</button>
-    <button onclick="aumentar()">+</button>
-
-    <script>
-        let valor = 0;
-
-        function atualizarTela() {
-            document.getElementById("numero").innerText = valor;
-        }
-
-        function aumentar() {
-            valor++;
-            atualizarTela();
-        }
-
-        function diminuir() {
-            valor--;
-            atualizarTela();
-        }
-    </script>
-
-</body>
-</html>
+botao.addEventListener("click", function() {
+    alert("Você clicou no botão!");
+});
