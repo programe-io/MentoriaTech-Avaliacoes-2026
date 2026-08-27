53b3d319d9b7723e0c07a3490547c087fabedff3
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-27 15:03:29 -0300

    publish(73025708883): 1

---
+++ 73025708883/aval-1/script.js
@@ -0,0 +1,64 @@
+let produtos = [];
+
+function errorCheck(desc, quant, valor) {
+    if (desc.length <5 ) {
+        throw new Error ("Descrição inválida. Min; 5 Caracteres");
+    }
+    if (quant < 0) {
+        throw new Error ("Quantidade invalida. Quantidade Min: 0");
+    }
+    if (valor < 0) {
+        throw new Error ("Valor inválido. Valor Min: 0");
+    }
+}
+
+function cadastrarProd(desc, quant, valor) {
+    errorCheck(desc, quant, valor);
+    let novoProd = {
+        "codigo": produtos.length + 1,
+        "desc": desc,
+        "quant": quant,
+        "valor": valor
+    }
+    produtos.push(novoProd);
+}
+
+function listarProd() {
+    console.log(produtos);
+}
+
+function updateValor(codigoProd, novoValor) {
+    if (novoValor < 0) {
+        throw new Error ("Valor inválido. Valor Min: 0");
+    }
+
+    const produto = produtos.find(prod => prod.codigo === codigoProd);
+
+    if (produto) {
+        produto.valor = novoValor;
+    } else {
+        throw new Error ("Produto não encontrado");
+    }
+}
+
+function updateQuant (codigoProd, novaQuant) {
+    if (novaQuant < 0) {
+        throw new Error ("Quantidade invalida, Quantidade Min: 0");
+    }
+
+    const produto = produtos.find(prod => prod.codigo === codigoProd);
+
+    if (produto) {
+        produto.quant = quant + novaQuant;
+    }
+    else {
+        throw new Error ("Produto não encontrado"); 
+    }
+}
+cadastrarProd("Batata", 100, 49,99);
+cadastrarProd("Cenoura", 215, 174,90);
+cadastrarProd("Arroz", 90, 44,99);
+cadastrarProd("feijão", 85, 34,90);
+listarProd();
+updateValor(2, 149,99);
+listarProd();
