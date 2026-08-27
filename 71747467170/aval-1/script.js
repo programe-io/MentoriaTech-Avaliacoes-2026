a3a8ae24f1fad787bbc8bbc6d6842400156ff116
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-27 14:21:47 -0300

    publish(71747467170): 1

--- 71747467170/aval-1/script.js
+++ 71747467170/aval-1/script.js
@@ -1,1 +1,7 @@
 
+function cadastrarProduto(descricao, quantidade, valor) {
+    // Descricao deve ter no mínimo 5 caracteres
+    if (descricao.length < 5) {
+        throw new Error("Descricao deve ter no mínimo cinco caracteres");
+    }
+}
