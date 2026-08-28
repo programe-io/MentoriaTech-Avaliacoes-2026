82324f1ce65a07c99ea8d2e303534cc91d2ddb82
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-08-28 10:35:10 -0300

    publish(73095177291): 1

--- 73095177291/aval-1/script.js
+++ 73095177291/aval-1/script.js
@@ -1,54 +1,31 @@
 let produtos = [];
 
 function validarProduto(descricao, quantidade, valor) {
-    if (descricao.length < 5) {
-        throw new Error("Descrição deve ter no mínimo cinco caracteres");
-    }
-    if (quantidade < 1) {
-        throw new Error("Quantidade deve ser maior que zero");
-    }
-    if (valor < 0) {
-        throw new Error("Valor deve ser maior ou igual a zero");
-    }
+    if (descricao.length < 5) throw new Error("Descrição deve ter no mínimo cinco caracteres");
+    if (quantidade < 1) throw new Error("Quantidade deve ser maior que zero");
+    if (valor < 0) throw new Error("Valor deve ser maior ou igual a zero");
 }
 
 function cadastrarProduto(descricao, quantidade, valor) {
     validarProduto(descricao, quantidade, valor);
-    let novoProduto = {
-        codigo: produtos.length + 1,
-        descricao: descricao,
-        quantidade: quantidade,
-        valor: valor
-    };
+    let novoProduto = {codigo: produtos.length + 1, descricao: descricao, quantidade: quantidade, valor: valor};
     produtos.push(novoProduto);
 }
 
-function listarProdutos() {
-    console.log(produtos);
-}
+function listarProdutos() { console.log(produtos); }
 
 function atualizarValor(codigoProduto, novoValor) {
-    if (novoValor < 0) {
-        throw new Error("Valor deve ser maior ou igual a zero");
-    }
+    if (novoValor < 0) throw new Error("Valor deve ser maior ou igual a zero");
     const produto = produtos.find(prod => prod.codigo === codigoProduto);
-    if (produto) {
-        produto.valor = novoValor;
-    } else {
-        throw new Error("Produto não encontrado");
-    }
+    if (produto) produto.valor = novoValor;
+    else throw new Error("Produto não encontrado");
 }
 
 function atualizarQuantidade(codigoProduto, novaQuantidade) {
-    if (novaQuantidade < 1) {
-        throw new Error("Quantidade deve ser maior que zero");
-    }
+    if (novaQuantidade < 1) throw new Error("Quantidade deve ser maior que zero");
     const produto = produtos.find(prod => prod.codigo === codigoProduto);
-    if (produto) {
-        produto.quantidade += novaQuantidade;
-    } else {
-        throw new Error("Produto não encontrado");
-    }
+    if (produto) produto.quantidade += novaQuantidade;
+    else throw new Error("Produto não encontrado");
 }
 
 listarProdutos();
