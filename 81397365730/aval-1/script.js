cac99de4a4af3aea0187d08eda603cfb8635ae15
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-01 15:00:26 -0300

    publish(81397365730): 1

---
+++ 81397365730/aval-1/script.js
@@ -0,0 +1,11 @@
+function mostrarMensagem() {
+
+        const mensagem = document.getElementById("mensagem");
+
+            mensagem.textContent =
+                    "🌷 Nana, continue se esforçando pelos seus sonhos! Você consegue! 💕";
+
+                    }
+
+                    console.log("Bem-vindo ao blog da Nana! 🌸");
+}
