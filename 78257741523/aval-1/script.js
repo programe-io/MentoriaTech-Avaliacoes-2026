7ae2c0a27b587a66ce42a37f41c004c3a6f2e8ff
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-04 08:30:25 -0300

    publish(78257741523): 1

---
+++ 78257741523/aval-1/script.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+  mensagem.textContent = "✨ O futuro começa com novas ideias e muita inovação!";
+  });
