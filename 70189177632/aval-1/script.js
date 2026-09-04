de802a0c29abbb92d03ec7c84b9eef721f11a027
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-04 10:57:31 -0300

    publish(70189177632): 1

---
+++ 70189177632/aval-1/script.js
@@ -0,0 +1,7 @@
+function lerMais(tituloPost) {
+    alert(`Você clicou para ler mais sobre: ${tituloPost}!\nFique ligado para novas postagens no blog de Thiago Felipe.`);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Blog de Jogos de Thiago Felipe carregado com sucesso!");
+});
