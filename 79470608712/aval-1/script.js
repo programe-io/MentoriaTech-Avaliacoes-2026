adb8dbf7f04f7d9aeb4ce4d511eae067013a03e3
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-05 15:43:29 -0300

    publish(79470608712): 1

---
+++ 79470608712/aval-1/script.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Meu Site</title>
+</head>
+
+<body>
+    <h1>Olá, seja bem-vindo!</h1>
+
+    <p>Este é o meu primeiro site criado em HTML.</p>
+
+    <button>Clique aqui</button>
+</body>
+</html>
