49ffec69b98adda0b152d8d3efedc255ea45cc11
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-08 14:12:23 -0300

    publish(71766725244): 2

---
+++ 71766725244/aval-2/script.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR" translate="no">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta http-equiv="cache-control" content="no-cache">
+  <meta http-equiv="expires" content="0">
+  <meta http-equiv="pragma" content="no-cache">
+  <meta name="google" content="notranslate">
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    rel="stylesheet" />
+  <link rel="icon" type="image/png" href="/favicon.png">
+  <title>PROGRAMA DE MENTORIAS</title>
+  <script type="module" crossorigin src="/assets/index-YhfXmj8M.js"></script>
+</head>
+
+<body>
+  <div id="root"></div>
+<script type="module" src="https://static.cloudflareinsights.com/beacon.min.js/v31edd6df95cf4e85bb4c19e7a9bdbcba1788362987495" integrity="sha512-iIg7k2xntmwu6/uSb5tpc/hySgZc4eoL31yB29W6tJFo2akwjPWcEqnCEdJvGexCL0KEQwVYv5BlowfhVz26hg==" data-cf-beacon='{"version":"2024.11.0","token":"cb746e2a1cce43b8bafb9943288eb7c8","r":1,"spa":2}' crossorigin="anonymous"></script>
+</body>
+
+</html>
