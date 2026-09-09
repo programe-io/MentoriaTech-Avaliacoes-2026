bba57f76351f1577d0e59fbddc8c765edb15545d
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-09 08:28:25 -0300

    publish(78528710580): 2

---
+++ 78528710580/aval-2/script.js
@@ -0,0 +1,8 @@
+function mostrarMensagem() {
+        const mensagem = document.getElementById("mensagem");
+
+            mensagem.textContent =
+                    "🌱 Preserve a natureza hoje para garantir um futuro melhor!";
+                    }
+                    
+}
