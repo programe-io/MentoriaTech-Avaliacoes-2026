2fa09a9f96bf35a4765f660886b2c5b22bd801a3
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-10 08:56:36 -0300

    publish(71497547792): 2

---
+++ 71497547792/aval-2/script.js
@@ -0,0 +1,64 @@
+let tarefas = [];
+let geradorCodigo = 0;
+
+function validarDadosTarefa(titulo, prioridade){
+    if(titulo.length < 5){
+        throw new Error('O título deve ter no mínimo 5 caracteres');
+    }
+
+    if(prioridade < 1 || prioridade > 3){
+        throw new Error('Informe uma prioridade entre 1 e 3');
+    }
+}
+
+function buscarTarefa(codigoTarefa){
+    const tarefaBuscada = tarefas.find(t => t.codigo === codigoTarefa);
+
+    if(!tarefaBuscada){
+        throw new Error('Código de tarefa não encontrado');
+    }
+
+    return tarefaBuscada;
+}
+
+function cadastrarTarefa(titulo, prioridade){
+    validarDadosTarefa(titulo, prioridade);
+
+    let tarefa = {
+        'codigo': ++geradorCodigo,
+        'titulo': titulo,
+        'prioridade': prioridade,
+        'status': true
+    };
+
+    tarefas.push(tarefa);
+}
+
+function listarTarefas(){
+    return tarefas;
+}
+
+function concluirTarefa(codigo){
+    let tarefa = buscarTarefa(codigo);
+
+    if(tarefa.status === false){
+        throw new Error('Tarefa já estava como concluída');
+    }
+
+    tarefa.status = false;
+}
+
+function alterarPrioridade(codigo, novaPrioridade){
+    let tarefa = buscarTarefa(codigo);
+    validarDadosTarefa(tarefa.titulo, novaPrioridade);
+
+    tarefa.prioridade = novaPrioridade;
+}
+
+cadastrarTarefa('Cadastrar Clientes', 1);
+cadastrarTarefa('Limpar banco de dados', 3);
+console.log(listarTarefas());
+
+concluirTarefa(2);
+
+console.log(listarTarefas());
