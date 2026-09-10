05cd6d008eca4921f7f2c0101e16a37d0225b0d7
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-10 08:30:52 -0300

    publish(72341484972): 1

---
+++ 72341484972/aval-1/script.js
@@ -0,0 +1,47 @@
+let produtos = [];
+function validarProdutos(descricao, quantidade, valor){
+	if(descricao.length<5){
+		console.log("Descrição deve ter no minimo 5 caracteres");
+	}
+	if(quantidade<1){
+		console.log("Quantidade deve ser maior que zero");
+	}
+	if(valor<0){
+		console.log("Valor deve ser maior que zero!");
+	}
+}
+function cadastrarProduto(descricao, quantidade, valor){
+	validarProduto(descricao, quantidade, valor);
+	let novoProduto = {
+		"codigo":produtos.length+1,
+		"descricao":descricao,
+		"quantidade":quantidade,
+		"valor":valor
+	}
+	produtos.push(novoProduto);
+}
+function listarProdutos(){
+	console.log(produtos);
+}
+function atualizarValor(codigoProduto,novoValor){
+	if (novoValor<0){
+		console.log("Valor deve ser maior que zero");
+	}
+	const produto = produtos.find(prod=>prod.codigo===codigoProduto);
+	if(produto){
+		produto.valor = novoValor;
+	} else{ 
+		console.log("Produto não encontrado");
+	}
+}
+function atualizarQuantidade(codigoProduto, novaQuantidade){
+	if (novaQuantidade<1){
+		console.log("Quantidade deve ser maior que zero");
+	}
+	const produto=produtos.find(prod=>prod.codigo===codigoProduto);
+	if (produto){
+		produto.quantidade+=novaQuantidade;
+	} else { 
+		console.log("Produto não encontrado");
+	}
+}
