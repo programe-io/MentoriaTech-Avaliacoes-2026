881271dbeff675120d616bd92aee0acb53ae049f
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-10 07:57:30 -0300

    publish(70258206489): 1

--- 70258206489/aval-1/script.js
+++ 70258206489/aval-1/script.js
@@ -1,31 +1,73 @@
-<script>
-    let senhaCorreta = "1234";
-    let tentativas = 3;
+let Produtos = [];
 
-    function verificarSenha() {
-        let senha = document.getElementById("senha").value;
-        let mensagem = document.getElementById("mensagem");
-
-        if (tentativas > 0) {
-
-            if (senha === senhaCorreta) {
-                mensagem.style.color = "green";
-                mensagem.innerHTML = "Bem-vindo! Acesso permitido.";
-            } else {
-                tentativas--;
-
-                if (tentativas > 0) {
-                    mensagem.style.color = "orange";
-                    mensagem.innerHTML = 
-                    "Senha incorreta! Restam " + tentativas + " tentativa(s).";
-                } else {
-                    mensagem.style.color = "red";
-                    mensagem.innerHTML = 
-                    "Acesso bloqueado! Número máximo de tentativas excedido.";
-
-                    document.getElementById("senha").disabled = true;
-                }
-            }
-        }
+function ValidarProdutos(Descricao, Quantidade, Valor) {
+    if (Descricao.length < 5) {
+        console.log("Descrição deve ter no mínimo 5 caracteres");
+        return false;
     }
-</script>
+
+    if (Quantidade < 1) {
+        console.log("Quantidade deve ser maior que zero");
+        return false;
+    }
+
+    if (Valor < 0) {
+        console.log("Valor deve ser maior ou igual a zero");
+        return false;
+    }
+
+    return true;
+}
+
+function CadastrarProduto(Descricao, Quantidade, Valor) {
+    if (!ValidarProdutos(Descricao, Quantidade, Valor)) {
+        return;
+    }
+
+    let novoProduto = {
+        Codigo: Produtos.length + 1,
+        Descrição: Descricao,
+        Quantidade: Quantidade,
+        Valor: Valor
+    };
+
+    Produtos.push(novoProduto);
+}
+
+function ListarProdutos() {
+    console.log(Produtos);
+}
+
+function AtualizarValor(CodigoProdutos, novoValor) {
+    if (novoValor < 0) {
+        console.log("Valor deve ser maior ou igual a zero");
+        return;
+    }
+
+    const produto = Produtos.find(
+        prod => prod.Codigo === CodigoProdutos
+    );
+
+    if (produto) {
+        produto.Valor = novoValor;
+    } else {
+        console.log("Produto não encontrado");
+    }
+}
+
+function AtualizarQuantidade(CodigoProdutos, novaQuantidade) {
+    if (novaQuantidade < 1) {
+        console.log("Quantidade deve ser maior que 0");
+        return;
+    }
+
+    const produto = Produtos.find(
+        prod => prod.Codigo === CodigoProdutos
+    );
+
+    if (produto) {
+        produto.Quantidade += novaQuantidade;
+    } else {
+        console.log("Produto não encontrado");
+    }
+}
