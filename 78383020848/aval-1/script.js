9b42f7cdae0a3a3b2050fbe245afaf2afb0e0750
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-11 08:06:25 -0300

    publish(78383020848): 1

---
+++ 78383020848/aval-1/script.js
@@ -0,0 +1,9 @@
+audio = document.getElementById("audio");
+const play = document.getElementById("play");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+
+const progress = document.getElementById("progress");
