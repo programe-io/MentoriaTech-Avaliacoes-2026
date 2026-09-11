9172d302c4f032eebd1fa78df68996eea5d870da
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-11 13:48:33 -0300

    publish(80308454596): 1

--- 80308454596/aval-1/script.js
+++ 80308454596/aval-1/script.js
@@ -1,38 +1,6 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
 
-
-const emojis = ["🐶","🐱","🐸","🦊","🐼","🐵","🐯","🦁"];
-let cartas = [...emojis, ...emojis]; // pares
-cartas.sort(() => Math.random() - 0.5);
-
-const tabuleiro = document.getElementById("tabuleiro");
-let primeira = null;
-let segunda = null;
-let travar = false;
-
-cartas.forEach((emoji) => {
-  const carta = document.createElement("div");
-  carta.classList.add("carta");
-  carta.dataset.valor = emoji;
-
-  carta.addEventListener("click", () => {
-    if (travar || carta.innerText) return;
-
-    carta.innerText = emoji;
-    carta.classList.add("virada");
-
-    if (!primeira) {
-      primeira = carta;
-    } else {
-      segunda = carta;
-      travar = true;
-
-      if (primeira.dataset.valor === segunda.dataset.valor) {
-        primeira = null;
-        segunda = null;
-        travar = false;
-      } else {
-        setTimeout(() => {
-          primeira.innerText = "";
-          segunda.innerText = "";
-          primeira.classList.remove("virada");
-          segunda.classList.remove("virada");
+botao.addEventListener("click", function() {
+    mensagem.textContent = "⚽ Futebol é paixão, emoção e alegria!";
+    });
