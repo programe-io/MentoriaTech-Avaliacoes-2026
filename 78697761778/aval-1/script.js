dc620851c268217d24efc000f57c4b214e56da4a
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-11 15:51:15 -0300

    publish(78697761778): 1

---
+++ 78697761778/aval-1/script.js
@@ -0,0 +1,6 @@
+Uma função simples que exibe uma mensagem de boas-vindas
+function saudarUsuario(nome) {
+    console.log("Olá, " + nome + "! Bem-vindo ao mundo do JavaScript.");
+    }
+
+    saudarUsuario("Desenvolvedor")
