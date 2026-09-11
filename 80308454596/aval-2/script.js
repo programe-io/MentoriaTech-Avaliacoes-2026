1b99a62e232cc362d9ea6a1059957898b77db3bf
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-11 13:51:22 -0300

    publish(80308454596): 2

---
+++ 80308454596/aval-2/script.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+    mensagem.textContent = "⚽ Futebol é paixão, emoção e alegria!";
+    });
