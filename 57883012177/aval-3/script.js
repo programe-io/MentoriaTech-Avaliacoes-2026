02575325554c16b5c06fc4307ad4a6beddbbe33f
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-11 14:26:02 -0300

    publish(57883012177): 3

---
+++ 57883012177/aval-3/script.js
@@ -0,0 +1,28 @@
+default
+
+s
+caches
+ 
+ XMLDocumentc
+ cachesx 
+
+
+  XMLDocumentx
+  cachesdf
+  debugger
+  z
+  XMLDocuments
+  pageXOffsetds
+  s
+  EncodedVideoChunkddf
+  cachesxx
+  speechSynthesiss
+  speechSynthesissx
+  XMLDocumentd
+  debuggerdc
+  debugger
+  decodeURIx 
+
+  XSLTProcessorsxcs
+  sxsc
+  ScriptProcessorNode
