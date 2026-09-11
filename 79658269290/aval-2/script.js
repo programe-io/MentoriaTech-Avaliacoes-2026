353ff70dd374fca43e04cea6841e89c8c1768bbd
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-11 14:36:26 -0300

    publish(79658269290): 2

---
+++ 79658269290/aval-2/script.js
@@ -0,0 +1,9 @@
+https://img.magnific.com/fotos-gratis/vista-da-configuracao-de-teclado-e-controlador-de-jogos-de-neon-iluminados_23-2149529367.jpg?semt=ais_hybrid&w=740&q=80body {
+  background-color: #222;
+  color: white;
+}
+
+.caixa {
+  background: linear-gradient(45deg, blue, purple);
+  opacity: 0.9;
+}
