c0bffc51369f9f2964f28dd1902af31aaa609879
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-11 07:58:58 -0300

    publish(81080347712): 1

---
+++ 81080347712/aval-1/script.js
@@ -0,0 +1,8 @@
+function mudarPagina() {
+        const texto = document.getElementById("texto");
+
+            texto.textContent = "Você clicou no botão! 🎉";
+
+                document.body.style.background = "#d9c7ff";
+                }
+}
