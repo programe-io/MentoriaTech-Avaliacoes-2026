cb9fafef87a5ddc72946d4d7238304d4aba9bfb4
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-11 14:44:05 -0300

    publish(79641978390): 2

---
+++ 79641978390/aval-2/script.js
@@ -0,0 +1,23 @@
+body {
+  font-size: 30px;
+}
+
+h1 {
+  font-size: 70px;
+}
+
+p {
+  font-size: 35px;
+}
+
+button {
+  width: 400px;
+  height: 100px;
+  font-size: 30px;
+  padding: 30px;
+}
+
+img {
+  width: 700px;
+  height: auto;
+}
