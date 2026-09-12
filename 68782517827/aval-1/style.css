4ce775dc31a45ce55c60829ad1d56159ed85aee5
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-12 18:05:47 -0300

    publish(68782517827): 1

---
+++ 68782517827/aval-1/style.css
@@ -0,0 +1,15 @@
+<a href="https://developer.mozilla.org/pt-BR/docs/Web/HTML">
+  HTML
+  </a>
+
+  <br><br>
+
+  <a href="https://developer.mozilla.org/pt-BR/docs/Web/CSS">
+    CSS
+    </a>
+
+    <br><br>
+
+    <a href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript">
+      JavaScript
+      </a>
