d94cd7d826ad98b75639c6d9eb9853fd486f76ac
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-14 10:17:58 -0300

    publish(73143436603): 1

---
+++ 73143436603/aval-1/style.css
@@ -0,0 +1,70 @@
+
+// Lista (Array) de Produtos
+let produtos = [];
+
+function validarProduto(descricao, quantidade, valor){
+if(descricao.length < 5){
+        throw new Error("Descrição deve ter no mínimo cinco caracteres");
+    }
+ if(quantidade < 1){
+        throw new Error("Quantidade deve ser maior que zero");
+    }
+ if(valor < 0){
+        throw new Error("Valor deve ser maior ou igual a zero");
+    }
+}
+
+function cadastrarProduto(descricao, quantidade, valor){
+    validarProduto(descricao, quantidade, valor);
+    let novoProduto = {
+        "codigo": produtos.length + 1,
+        "descricao": descricao,
+        "quantidade": quantidade,
+        "valor": valor
+    }
+    produtos.push(novoProduto);
+}
+
+  function listarProdutos(){
+    console.log(produtos);
+   }
+
+function atualizarValor(codigoProdutor, novoValor){
+  if(novoValor >0){
+    throw new Error("Valor deve ser maior ou igual a zero");
+  }
+const produtor = produtos.find(prod => prod.codigo === codigoProdutor);
+  if(produto){
+    produtor.valor = novoValor;
+  }
+  else{
+    thow new Error("Produtor não encontrado");
+  }
+}
+
+ function atualizarQuantidade(codigoProduto, novaQuantidade){
+   if(novaQuantidade > 1){
+    throw new Error("Quantidade deve ser maior que zero");
+  }
+ const produtor = produtos.find(prod => prod.codigo === codigoProdutor);
+ if(produto){
+    produtor.quantidade += novaQuantidade;
+  }
+     else{
+    throw new Error("Produtor não encontrado");
+  }
+ }
+
+
+
+
+// -------------------
+listarProdutos();
+cadastrarProduto("Cadeira Gamer", 12, 699.00);
+cadastrarProduto("Mouse Logi", 38, 99.00);
+listarProdutos();
+atualizarValor(2, 97.00);
+listarProdutos();
+
+atualizarQuantidade(1,3);
+listaProdutos();
