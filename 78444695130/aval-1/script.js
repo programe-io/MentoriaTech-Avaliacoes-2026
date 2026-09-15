234611eeb4449e9e04d85625562e01fe07f28ef0
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-15 14:52:38 -0300

    publish(78444695130): 1

---
+++ 78444695130/aval-1/script.js
@@ -0,0 +1,12 @@
+```javascript
+// Mensagem de boas-vindas
+function boasVindas() {
+    alert("Olá! Bem-vindo ao Meu Blog do Danilo!");
+}
+
+// Mudar o texto do post
+function mudarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+        "Obrigado por visitar meu blog! 😊";
+}
+```
