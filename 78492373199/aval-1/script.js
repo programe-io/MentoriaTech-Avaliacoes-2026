18bab30f5343b3e39ccba2155c5345c91c6db6fa
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-15 14:55:22 -0300

    publish(78492373199): 1

---
+++ 78492373199/aval-1/script.js
@@ -0,0 +1,12 @@
+function mostrarMensagem() {
+    alert("Bem-vindo à Sertão Agro!");
+}
+
+function adicionarProduto(produto) {
+    alert(produto + " foi adicionado!");
+}
+
+function mostrarContato() {
+    document.getElementById("resultado").innerHTML =
+        "Entre em contato pelo telefone: (89) 99999-9999";
+}
