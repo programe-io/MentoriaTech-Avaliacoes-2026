3fe7b8f7cba50b4afbf7e0978db5bc8161b341e9
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-16 08:14:21 -0300

    publish(46130874375): 1

---
+++ 46130874375/aval-1/script.js
@@ -0,0 +1,17 @@
+/* ==========================
+   MENSAGEM DOS PROJETOS
+========================== */
+
+function mostrarMensagem(projeto) {
+    alert("Você selecionou o " + projeto + "!");
+}
+
+
+/* ==========================
+   MOSTRAR CONTATO 8640028922
+========================== */
+
+function mostrarContato() {
+    let mensagem = document.getElementById("mensagem");
+    mensagem.innerHTML = "Entre em contato pelo e-mail: luanrafael22@gmail.com";
+}
