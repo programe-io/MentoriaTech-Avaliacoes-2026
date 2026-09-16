6cfc453fa8cabe812e94d027002afc4461767f7b
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-16 08:07:54 -0300

    publish(56338159938): 1

---
+++ 56338159938/aval-1/script.js
@@ -0,0 +1,17 @@
+/* ==========================
+   MENSAGEM DOS PROJETOS
+========================== */
+
+function mostrarMensagem(projeto) {
+    alert("Você selecionou o " + projeto + "!");
+}
+
+
+/* ==========================
+   MOSTRAR CONTATO
+========================== */
+
+function mostrarContato() {
+    let mensagem = document.getElementById("mensagem");
+    mensagem.innerHTML = "Entre em contato pelo e-mail: sonoffmesmo@gmail.com";
+}
