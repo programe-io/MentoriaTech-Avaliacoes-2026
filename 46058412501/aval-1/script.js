debf4220d66cefb261543afc537ba1c2cc74bf3b
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-16 08:06:10 -0300

    publish(46058412501): 1

---
+++ 46058412501/aval-1/script.js
@@ -0,0 +1,8 @@
+function mostrarMensagem(projeto) {
+    alert("Você clicou em " + projeto + "!");
+}
+
+function mostrarContato() {
+    document.getElementById("mensagem").innerHTML =
+        "Entre em contato pelas minhas redes sociais!";
+}
