b4cd62cc7a0fa184f0c504aef27438e8dcd0108d
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-16 08:10:37 -0300

    publish(64414112311): 1

---
+++ 64414112311/aval-1/script.js
@@ -0,0 +1,18 @@
+
+/* ==========================
+   MENSAGEM DOS PROJETOS
+========================== */
+
+function mostrarMensagem(projeto) {
+    alert("Você selecionou o " + projeto + "!");
+}
+
+
+/* ==========================
+   MOSTRAR CONTATO
+========================== */
+
+function mostrarContato() {
+    let mensagem = document.getElementById("mensagem");
+    mensagem.innerHTML = "Entre em contato pelo e-mail: gabrielgabrielenzo14@gmail.com";
+} 
