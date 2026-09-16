a8d8887688f5aee286768ca7498765575a957913
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-16 08:19:27 -0300

    publish(46337375505): 1

---
+++ 46337375505/aval-1/script.js
@@ -0,0 +1,19 @@
+/* ==========================
+   MENSAGEM DOS PROJETOS
+========================== */
+
+function mostrarMensagem(projeto) {
+    alert("Você selecionou o " + projeto + "!");
+}
+
+
+/* ==========================
+   MOSTRAR CONTATO
+========================== */
+
+function mostrarContato() {
+    let mensagem = document.getElementById("mensagem");
+
+    mensagem.innerHTML =
+        "Entre em contato pelo e-mail: andressa@gmail.com";
+}
