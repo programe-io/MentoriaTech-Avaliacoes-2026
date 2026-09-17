8d4d9b4e0c6942524122a8f2083f7b372760c784
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-17 20:14:39 -0300

    publish(78103418838): 3

---
+++ 78103418838/aval-3/script.js
@@ -0,0 +1,7 @@
+// MeuApp - Anabelly
+console.log("MeuApp carregado com sucesso!");
+document.querySelectorAll("button").forEach(btn => {
+  btn.addEventListener("click", () => {
+      alert("Teste Grátis - Funcionalidade em desenvolvimento");
+        });
+        });
