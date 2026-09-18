199d1ff21e2b664bb2d2dc7f43b1c0f6979750ab
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-18 08:43:34 -0300

    publish(79783751905): 1

---
+++ 79783751905/aval-1/script.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+  mensagem.textContent = "🌟 A aventura começou! Explore a floresta e descubra seus segredos!";
+  });
