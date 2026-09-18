2d7c5213b8c3583d2a32fb3ea2efc479587f7e22
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-18 09:10:57 -0300

    publish(78630981504): 1

---
+++ 78630981504/aval-1/script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+  mensagem.textContent =
+      "🏰✨ Bem-vindo ao Castelo Encantado! Uma grande aventura começa agora!";
+      });
