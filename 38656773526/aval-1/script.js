a246ea13f720024711999ca9b37e94ab8a9151df
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-18 15:02:26 -0300

    publish(38656773526): 1

---
+++ 38656773526/aval-1/script.js
@@ -0,0 +1,37 @@
+<div class="container">
+  <h1>Gerenciador de Tarefas</h1>
+
+    <div class="menu">
+        <button onclick="abrirCadastro()">
+              Cadastrar tarefa
+                  </button>
+
+                      <button onclick="listarTarefas()">
+                            Listar tarefas
+                                </button>
+
+                                    <button onclick="abrirConclusao()">
+                                          Marcar como concluída
+                                              </button>
+
+                                                  <button onclick="abrirAlteracaoPrioridade()">
+                                                        Alterar prioridade
+                                                            </button>
+
+                                                                <button class="botao-sair" onclick="sairSistema()">
+                                                                      Sair
+                                                                          </button>
+                                                                            </div>
+
+                                                                              <div id="mensagem"></div>
+
+                                                                                <section id="areaFormulario"></section>
+
+                                                                                  <section class="tarefas">
+                                                                                      <h2>Tarefas cadastradas</h2>
+
+                                                                                          <div id="listaTarefas">
+                                                                                                <p>Nenhuma tarefa cadastrada.</p>
+                                                                                                    </div>
+                                                                                                      </section>
+                                                                                                      </div>
