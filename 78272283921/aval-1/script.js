37231dd435d172bf13ab5085df28ddea060aec0c
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-18 10:07:31 -0300

    publish(78272283921): 1

---
+++ 78272283921/aval-1/script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+  mensagem.textContent =
+      "🌌 Missão iniciada! Prepare-se para descobrir os segredos da galáxia!";
+      });
