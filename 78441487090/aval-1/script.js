1c7f8ca49ae6b55aac434c5ef8f27ebae3b710ac
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-18 09:24:18 -0300

    publish(78441487090): 1

---
+++ 78441487090/aval-1/script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+  mensagem.textContent =
+      "🎮 A aventura começou! Prepare-se para entrar no Mundo dos Games!";
+      });
