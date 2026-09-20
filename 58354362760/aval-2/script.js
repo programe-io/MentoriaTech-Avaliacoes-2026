aa9b9460102e03c186b62b1e167d4036016f8926
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-20 20:06:50 -0300

    publish(58354362760): 2

---
+++ 58354362760/aval-2/script.js
@@ -0,0 +1,9 @@
+console.log("Página da Ariadiny carregada com sucesso!");
+
+document.addEventListener("DOMContentLoaded", function() {
+    const cartao = document.querySelector(".cartao");
+        
+            cartao.addEventListener("click", function() {
+                    alert("Obrigada por visitar minha segunda página! - Ariadiny 💖");
+                        });
+                        });
