bc9ebe3a340f655b0aa4f260a07a6e1ffbfc6bb9
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-21 14:44:13 -0300

    publish(73268149499): 2

---
+++ 73268149499/aval-2/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btnSaudacao = document.getElementById('btnSaudacao');
+
+    btnSaudacao.addEventListener('click', () => {
+        alert('Valeu pela visita! — Veloso Junior (CETI Paulo Freire)');
+    });
+});
