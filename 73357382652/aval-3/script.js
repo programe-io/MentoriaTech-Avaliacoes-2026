faca48496d05e4286ff527d9388244148d5fa427
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-22 14:10:02 -0300

    publish(73357382652): 3

---
+++ 73357382652/aval-3/script.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("botaoContato");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function () {
+    mensagem.textContent = "Obrigado por visitar meu portfólio! 🚀";
+});
