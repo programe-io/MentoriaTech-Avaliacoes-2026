cb2b02085d030f0efd35cc61b51fb8b7b21f355d
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-22 11:41:09 -0300

    publish(73178685197): 2

---
+++ 73178685197/aval-2/script.js
@@ -0,0 +1,29 @@
+let meuArray = [];
+
+meuArray.unshift(3);
+meuArray.unshift(6);
+meuArray.unshift(9);
+meuArray.unshift(12);
+meuArray.unshift(15);
+meuArray.unshift(18);
+meuArray.unshift(21);
+meuArray.unshift(24);
+
+console.log("Array depois dos unshift:");
+console.log(meuArray);
+
+meuArray.shift();
+console.log("Depois do primeiro shift:");
+console.log(meuArray);
+
+meuArray.shift();
+console.log("Depois do segundo shift:");
+console.log(meuArray);
+
+meuArray.shift();
+console.log("Depois do terceiro shift:");
+console.log(meuArray);
+
+meuArray.shift();
+console.log("Depois do quarto shift:");
+console.log(meuArray);
