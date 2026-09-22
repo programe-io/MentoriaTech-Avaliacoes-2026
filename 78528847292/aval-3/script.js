5d954ef2e98993a124c3918704df3b9c88162894
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-22 18:11:46 -0300

    publish(78528847292): 3

---
+++ 78528847292/aval-3/script.js
@@ -0,0 +1,9 @@
+function mostrarMensagem(nome) {
+      alert("Você escolheu o livro: " + nome);
+      }
+
+      function pesquisar() {
+        let texto = document.getElementById("pesquisa").value;
+          alert("Você pesquisou por: " + texto);
+          }
+}
