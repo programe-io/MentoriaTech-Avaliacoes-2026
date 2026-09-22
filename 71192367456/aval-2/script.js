ade4dbbd84266e0b165f406bacb0b7a25e3569d6
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-22 12:35:09 -0300

    publish(71192367456): 2

---
+++ 71192367456/aval-2/script.js
@@ -0,0 +1,18 @@
+let numero = 17;
+
+switch (numero) {
+    case 53:
+        console.log("O valor é igual a 53");
+        break;
+
+    case 10:
+        console.log("O valor é igual a 10");
+        break;
+
+    case 15:
+        console.log("O valor é igual a 15");
+        break;
+
+    default:
+        console.log("Não tenho um case para o valor especificado");
+\}$0
