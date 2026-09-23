102766df0cb14089d775cd1eca41c80d4f4b8d61
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-23 20:14:27 -0300

    publish(50146739590): 1

---
+++ 50146739590/aval-1/script.js
@@ -0,0 +1,8 @@
+console.log("Página carregada com sucesso! 🚀");
+
+const botao = document.querySelector("button");
+if (botao) {
+  botao.addEventListener("click", function() {
+      alert("Mensagem enviada! Obrigado pelo contato! ✅");
+        });
+        }
