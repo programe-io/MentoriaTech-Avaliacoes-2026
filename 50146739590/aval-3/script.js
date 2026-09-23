bafb24b225213e03718037ec729e4dfbc65ebd1c
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-23 20:36:43 -0300

    publish(50146739590): 3

---
+++ 50146739590/aval-3/script.js
@@ -0,0 +1,8 @@
+console.log("✅ Portfólio carregado com sucesso!");
+
+const botao = document.getElementById("botao-contato");
+
+botao.addEventListener("click", function() {
+  alert("Obrigado pelo interesse! 📩 Estou evoluindo a cada dia! — Lucca 💪🚀");
+  });
+  
