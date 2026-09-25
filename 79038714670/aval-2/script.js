711254b42f1b2196ddd22136322ac0098341d088
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-25 13:36:55 -0300

    publish(79038714670): 2

---
+++ 79038714670/aval-2/script.js
@@ -0,0 +1,4 @@
+function escolherTime(nomeDoTime) {
+    const mensagem = document.getElementById('mensagem-torcida');
+    mensagem.innerText = "Boa escolha! Agora você faz parte da torcida oficial do " + nomeDoTime + "! 📣⚽";
+}
