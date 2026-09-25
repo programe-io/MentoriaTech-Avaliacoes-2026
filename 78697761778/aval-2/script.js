15501f2b409f9ae5194c28963b7315246e3333ae
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-25 08:47:42 -0300

    publish(78697761778): 2

---
+++ 78697761778/aval-2/script.js
@@ -0,0 +1,12 @@
+/**
+ *  * Gerencia as interações dinâmicas dos botões na página.
+  * @param {string} pet - O tipo de animal selecionado ('gato' ou 'cachorro').
+   */
+   function interagir(pet) {
+       if (pet === 'gato') {
+               alert("Miau! O gatinho ronronou e voltou a dormir debaixo de uma fresta de sol. 🐾💤");
+                   } else if (pet === 'cachorro') {
+                           alert("Au au! O cãozinho começou a abanar o rabo vigorosamente esperando você jogar a bolinha! 🎾🐶");
+                               }
+                               }
+ */
