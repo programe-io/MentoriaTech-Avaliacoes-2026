942b648e739dfcd149e2815bf05034d09ac949fc
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-25 09:22:45 -0300

    publish(78265256523): 2

---
+++ 78265256523/aval-2/script.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+        <title>Minha Primeira Página</title>
+        </head>
+        <body>
+            <h1>Olá, mundo!</h1>
+                <p>Meu nome é estudante do Programe.io e esta é minha primeira página em HTML.</p>
+                    
+                        <h2>O que estou aprendendo</h2>
+                            <ul>
+                                    <li>HTML</li>
+                                            <li>Estrutura de páginas</li>
+                                                    <li>Tags e títulos</li>
+                                                        </ul>
+
+                                                            <p>Estou animado para continuar!</p>
+                                                            </body>
+                                                            </html>
