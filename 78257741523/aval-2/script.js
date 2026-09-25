3a3f2b0feda9864b84b2e1bfecab0f9d4d5b9462
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-25 08:33:37 -0300

    publish(78257741523): 2

---
+++ 78257741523/aval-2/script.js
@@ -0,0 +1,7 @@
+function mostrarMensagem() {
+      const mensagem = document.getElementById("mensagem");
+
+        mensagem.innerHTML =
+            "✨ Bem-vindo à Cidade do Futuro! A tecnologia pode transformar o nosso mundo.";
+            }
+}
