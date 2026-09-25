492055e2c2a8aa3ba351b23fde85a73b389f7de7
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2026-09-25 13:40:25 -0300

    publish(79038714670): 3

---
+++ 79038714670/aval-3/script.js
@@ -0,0 +1,9 @@
+// Aguarda o carregamento do documento
+document.addEventListener("DOMContentLoaded", () => {
+    const botaoAgendar = document.getElementById("btnAgendar");
+
+    // Adiciona evento de clique para simular o agendamento
+    botaoAgendar.addEventListener("click", () => {
+        alert("🗓️ Redirecionando para o nosso sistema de agendamentos no WhatsApp!");
+    });
+});
